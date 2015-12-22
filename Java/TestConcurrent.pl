5573db9bc775ec8b6388e4d9cfa858b3b981815a
Flavio S. Glock <fglock@gmail.com>
2015-12-22 12:51:55 +0100

    Perlito5 - docs - discourage indirect object notation in Perlito5-Java examples

---
+++ Java/TestConcurrent.pl
@@ -0,0 +1,38 @@
+package Java::Thread {
+    import => "java.lang.Thread"
+};
+
+package ConcurrentLinkedQueue::Of::String {
+    import    => "java.util.concurrent.ConcurrentLinkedQueue",
+    java_type => "ConcurrentLinkedQueue<PlObject>",
+};
+
+my $global_queue = ConcurrentLinkedQueue::Of::String->new();
+
+my Java::Thread $producer = Java::Thread->new(
+    sub {
+        my ConcurrentLinkedQueue::Of::String $queue = $global_queue->to_ConcurrentLinkedQueueOfString();
+        my $x = 1;
+        while(1) {
+            $x++;
+            $queue->add($x);   
+            sleep (1);
+        }
+    }
+);
+my Java::Thread $consumer = Java::Thread->new(
+    sub {
+        while(1) {
+            my ConcurrentLinkedQueue::Of::String $queue = $global_queue->to_ConcurrentLinkedQueueOfString();
+            my $x = $queue->poll();
+            if (defined $x) {
+                print "consumer got $x\n";
+            }
+            sleep (1);
+        }
+    }
+);
+
+$producer->start();
+$consumer->start();
+
