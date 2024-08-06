a526fb4dc14e2be00a68fffd6ed38cb602dec874
Flavio S. Glock <fglock@gmail.com>
2024-08-06 12:18:30 +0200

    array/hash wip

---
+++ src/main/java/org/perlonjava/RuntimeList.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * The RuntimeList class simulates a Perl list.
+ *
+ */
+public class RuntimeList {
+    private List<Runtime> elements;
+
+    // Constructor
+    public RuntimeList() {
+        this.elements = new ArrayList<>();
+    }
+
+    // Get the size of the list
+    public int size() {
+        return elements.size();
+    }
+
+    // Get the scalar value of the array
+    public Runtime scalar() {
+        if (elements.isEmpty()) {
+            return new Runtime(); // Return undefined if empty
+        }
+        return elements.get(elements.size() - 1);
+    }
+
+    // Join the list into a string
+    public String join(String delimiter) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < elements.size(); i++) {
+            if (i > 0) {
+                sb.append(delimiter);
+            }
+            sb.append(elements.get(i).toString());
+        }
+        return sb.toString();
+    }
+
+    // Convert the list to a string (for debugging purposes)
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < elements.size(); i++) {
+            if (i > 0) {
+                sb.append(", ");
+            }
+            sb.append(elements.get(i).toString());
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
