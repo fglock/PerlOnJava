4eb05d03e6e764791f3e2d4e171fc7ec795ddfb7
Flavio S. Glock <fglock@gmail.com>
2024-08-07 09:04:54 +0200

    list context wip

---
+++ src/main/java/org/perlonjava/AbstractRuntimeObject.java
@@ -0,0 +1,14 @@
+
+/**
+ * The AbstractRuntimeObject class is a base class for scalar, hash, and array variables.
+ *
+ */
+public abstract class AbstractRuntimeObject implements ContextProvider {
+
+  // Add the object to a list
+  public void addToList(RuntimeList list) {
+      list.add(this);
+  }
+
+}
+
