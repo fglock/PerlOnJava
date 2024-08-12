28b4b8ef876fe14278507ecaa2ff46d5cb604d61
Flavio S. Glock <fglock@gmail.com>
2024-08-12 20:21:53 +0200

    global array, hash

--- src/main/java/org/perlonjava/Namespace.java
+++ src/main/java/org/perlonjava/Namespace.java
@@ -7,8 +7,19 @@ import java.util.*;
 public class Namespace {
 
   private static Map<String, Runtime> globalVariables = new HashMap<>();
+  private static Map<String, RuntimeArray> globalArrays = new HashMap<>();
+  private static Map<String, RuntimeHash> globalHashes = new HashMap<>();
 
   // Static methods
+
+  public static void initializeGlobals() {
+      getGlobalVariable("$@");    // initialize $@ to "undef"
+      getGlobalVariable("$_");    // initialize $_ to "undef"
+      getGlobalVariable("$\"").set(" ");    // initialize $_ to " "
+      getGlobalArray("@INC");
+      getGlobalHash("%INC");
+  }
+
   public static Runtime setGlobalVariable(String key, Runtime value) {
       Runtime var = globalVariables.get(key);
       if (var == null) {
@@ -38,6 +49,24 @@ public class Namespace {
 
   public static boolean existsGlobalVariable(String key) {
       return globalVariables.containsKey(key);
+  }
+
+  public static RuntimeArray getGlobalArray(String key) {
+      RuntimeArray var = globalArrays.get(key);
+      if (var == null) {
+        var = new RuntimeArray();
+        globalArrays.put(key, var);
+      }
+      return var;
+  }
+
+  public static RuntimeHash getGlobalHash(String key) {
+      RuntimeHash var = globalHashes.get(key);
+      if (var == null) {
+        var = new RuntimeHash();
+        globalHashes.put(key, var);
+      }
+      return var;
   }
 }
 
