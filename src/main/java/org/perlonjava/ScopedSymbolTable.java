325ea7b5f428fe2f33487022780f1dd28fc97151
Flavio S. Glock <fglock@gmail.com>
2024-08-15 12:14:28 +0200

    move SymbolTable to ScopedSymbolTable as inner class

--- src/main/java/org/perlonjava/ScopedSymbolTable.java
+++ src/main/java/org/perlonjava/ScopedSymbolTable.java
@@ -1,16 +1,13 @@
 package org.perlonjava;
 
-import java.util.Stack;
-import java.util.Map;
-import java.util.Set;
-import java.util.HashSet;
-import java.util.TreeMap;
+import java.util.*;
 
 /**
  * A scoped symbol table that supports nested scopes for variable declarations.
  */
 class ScopedSymbolTable {
-    // A stack to manage nested scopes of symbol tables
+    // A stack to manage nested scopes of symbol tables.
+    // SymbolTable is an inner class.
     private final Stack<SymbolTable> stack = new Stack<>();
     
     /**
@@ -127,6 +124,44 @@ class ScopedSymbolTable {
             st.addVariable(visibleVariables.get(index));
         }
         return st;
+    }
+
+    /**
+     * A simple symbol table that maps variable names to unique integer indices.
+     */
+    static class SymbolTable {
+        // A map to store variable names and their corresponding indices
+        public Map<String, Integer> table = new HashMap<>();
+
+        // A counter to generate unique indices for variables
+        public int index = 0;
+
+        /**
+         * Adds a variable to the symbol table if it does not already exist.
+         *
+         * @param name The name of the variable to add.
+         * @return The index of the variable in the symbol table.
+         */
+        public int addVariable(String name) {
+            // Check if the variable is not already in the table
+            if (!table.containsKey(name)) {
+                // Add the variable with a unique index
+                table.put(name, index++);
+            }
+            // Return the index of the variable
+            return table.get(name);
+        }
+
+        /**
+         * Retrieves the index of a variable from the symbol table.
+         *
+         * @param name The name of the variable to look up.
+         * @return The index of the variable, or -1 if the variable is not found.
+         */
+        public int getVariableIndex(String name) {
+            // Return the index of the variable, or -1 if not found
+            return table.getOrDefault(name, -1);
+        }
     }
 }
 
