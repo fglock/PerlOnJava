9703e5aae5cf44c36de813281af208995a6ef6b5
Flavio S. Glock <fglock@gmail.com>
2024-08-15 10:55:11 +0200

    rename ContextProvider to RuntimeDataProvider

--- src/main/java/org/perlonjava/AbstractRuntimeObject.java
+++ src/main/java/org/perlonjava/AbstractRuntimeObject.java
@@ -4,7 +4,7 @@ package org.perlonjava;
  * The AbstractRuntimeObject class is a base class for scalar, hash, and array variables.
  *
  */
-public abstract class AbstractRuntimeObject implements ContextProvider {
+public abstract class AbstractRuntimeObject implements RuntimeDataProvider {
 
   // Add the object to a list
   public void addToList(RuntimeList list) {
