08d638bd44200f440cc48a2667cbcb5e658b1be8
Flavio S. Glock <fglock@gmail.com>
2024-08-19 15:49:20 +0200

    cmp operators

--- src/main/java/org/perlonjava/RuntimeScalar.java
+++ src/main/java/org/perlonjava/RuntimeScalar.java
@@ -623,6 +623,25 @@ public class RuntimeScalar extends RuntimeBaseEntity implements RuntimeScalarRef
         }
     }
 
+    public RuntimeScalar spaceship(RuntimeScalar arg2) {
+        RuntimeScalar arg1 = this;
+        if (arg1.type == RuntimeScalarType.STRING) {
+            arg1 = arg1.parseNumber();
+        }
+        if (arg2.type == RuntimeScalarType.STRING) {
+            arg2 = arg2.parseNumber();
+        }
+        if (arg1.type == RuntimeScalarType.DOUBLE || arg2.type == RuntimeScalarType.DOUBLE) {
+            return new RuntimeScalar(Double.compare(arg1.getDouble(), arg2.getDouble()));
+        } else {
+            return new RuntimeScalar(Integer.compare(arg1.getInt(), arg2.getInt()));
+        }
+    }
+
+    public RuntimeScalar cmp(RuntimeScalar arg2) {
+        return new RuntimeScalar(this.toString().compareTo(arg2.toString()));
+    }
+
     public RuntimeScalar eq(RuntimeScalar arg2) {
         return new RuntimeScalar(this.toString().equals(arg2.toString()));
     }
