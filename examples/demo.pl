9ecc4e841d6688fd4bda3f29e1332c9c3f9e0d10
Flavio S. Glock <fglock@gmail.com>
2024-08-29 16:20:59 +0200

    closure variable: cache variable list

--- examples/demo.pl
+++ examples/demo.pl
@@ -11,6 +11,98 @@ print "not " if $x != 15; say "ok 1 - \$x is 15";
 $a = 12;
 print "not " if $a != 12; say "ok 2 - \$a is 12";
 
+
+############################
+#  List assignment in scalar context
+
+{
+# List assignment in scalar context returns the number of elements produced by the expression on the right side of the assignment
+
+# Test with a static list
+my @array = (10, 20, 30, 40, 50);
+my $count = () = @array;
+print "not " if $count != 5; say "ok # List assignment in scalar context returned '$count'";
+
+@array = ();
+$count = () = @array;
+print "not " if $count != 0; say "ok # List assignment in scalar context returned '$count'";
+
+# Test with a list containing variables
+my $var1 = 10;
+my $var2 = 20;
+my $var3 = 30;
+$count = () = ($var1, $var2, $var3);
+print "not " if $count != 3; say "ok # List assignment with variables in scalar context returned '$count'";
+
+# Test with a mixed list of variables and literals
+my $var4 = 40;
+@array = ($var1, $var2, $var3, $var4, 50, 60);
+$count = () = @array;
+print "not " if $count != 6; say "ok # List assignment with mixed variables and literals in scalar context returned '$count'";
+
+# Test with non-empty left-hand side
+my ($first, $second);
+@array = (1, 2, 3, 4, 5);
+$count = ($first, $second) = @array;
+print "not " if $count != 5; say "ok # List assignment with non-empty left-hand side returned '$count'";
+print "not " if $first != 1; say "ok # First variable assigned correctly with value '$first'";
+print "not " if $second != 2; say "ok # Second variable assigned correctly with value '$second'";
+
+@array = (10, 20);
+$count = ($first, $second) = @array;
+print "not " if $count != 2; say "ok # List assignment with non-empty left-hand side returned '$count'";
+print "not " if $first != 10; say "ok # First variable assigned correctly with value '$first'";
+print "not " if $second != 20; say "ok # Second variable assigned correctly with value '$second'";
+
+@array = (100);
+$count = ($first, $second) = @array;
+print "not " if $count != 1; say "ok # List assignment with non-empty left-hand side returned '$count'";
+print "not " if $first != 100; say "ok # First variable assigned correctly with value '$first'";
+## TODO print "not " if defined $second; say "ok # Second variable is undefined as expected";
+}
+
+############################
+#  List assignment in scalar context with lvalue array and hash
+
+{
+
+# Test with non-empty left-hand side including an array
+my ($first, $second, @lvalue_array);
+my @array = (1, 2, 3, 4, 5);
+my $count = ($first, $second, @lvalue_array) = @array;
+print "not " if $count != 5; say "ok # List assignment with lvalue array returned '$count'";
+print "not " if $first != 1; say "ok # First variable assigned correctly with value '$first'";
+print "not " if $second != 2; say "ok # Second variable assigned correctly with value '$second'";
+print "not " if "@lvalue_array" ne "3 4 5"; say "ok # Lvalue array assigned correctly with values '@lvalue_array'";
+
+@array = (10, 20);
+$count = ($first, $second, @lvalue_array) = @array;
+print "not " if $count != 2; say "ok # List assignment with lvalue array returned '$count'";
+print "not " if $first != 10; say "ok # First variable assigned correctly with value '$first'";
+print "not " if $second != 20; say "ok # Second variable assigned correctly with value '$second'";
+print "not " if "@lvalue_array" ne ""; say "ok # Lvalue array assigned correctly with values '@lvalue_array'";
+
+# Test with non-empty left-hand side including a hash
+my %lvalue_hash;
+@array = (1, 2, 3, 4, 5);
+$count = ($first, $second, %lvalue_hash) = @array;
+print "not " if $count != 5; say "ok # List assignment with lvalue hash returned '$count'";
+print "not " if $first != 1; say "ok # First variable assigned correctly with value '$first'";
+print "not " if $second != 2; say "ok # Second variable assigned correctly with value '$second'";
+## TODO print "not " if keys %lvalue_hash != 1 || $lvalue_hash{3} != 4; say "ok # Lvalue hash assigned correctly with keys and values";
+
+@array = (10, 20);
+$count = ($first, $second, %lvalue_hash) = @array;
+print "not " if $count != 2; say "ok # List assignment with lvalue hash returned '$count'";
+print "not " if $first != 10; say "ok # First variable assigned correctly with value '$first'";
+print "not " if $second != 20; say "ok # Second variable assigned correctly with value '$second'";
+## TODO print "not " if keys %lvalue_hash != 0; say "ok # Lvalue hash assigned correctly with keys and values";
+
+}
+
+###################
+# Basic Syntax
+
 # Test anonymous subroutine
 my @result = sub { return @_ }->(1, 2, 3);
 print "not " if "@result" ne "1 2 3"; say "ok 3 - anonymous subroutine returned '@result'";
