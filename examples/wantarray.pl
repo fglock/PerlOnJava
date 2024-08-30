eaf9a161934cf54501b89d5e1ce74392f0c5ce2c
Flavio S. Glock <fglock@gmail.com>
2024-08-30 12:00:29 +0200

    add tests

--- examples/wantarray.pl
+++ examples/wantarray.pl
@@ -5,43 +5,36 @@
 ###################
 # wantarray Tests
 
+sub wantarray_as_string {
+    !defined $_[0] ? "void" : $_[0] ? "list" : "scalar"
+}
+
 # Test wantarray in list context
-my @list_context_result = sub { return wantarray ? "list" : "scalar" }->();
+my @list_context_result = sub { return wantarray_as_string(wantarray) }->();
 print "not " if "@list_context_result" ne "list"; say "ok # wantarray in list context returned '@list_context_result'";
 
 # Test wantarray in scalar context
-my $scalar_context_result = sub { return wantarray ? "list" : "scalar" }->();
+my $scalar_context_result = sub { return wantarray_as_string(wantarray) }->();
 print "not " if $scalar_context_result ne "scalar"; say "ok # wantarray in scalar context returned '$scalar_context_result'";
 
 # Test wantarray in void context
 my $void_context_result;
 {
-    sub { $void_context_result = wantarray ? "list" : "scalar" }->();
+    sub { $void_context_result = wantarray_as_string(wantarray) }->();
 }
-print "not " if $void_context_result ne "scalar"; say "ok # wantarray in void context returned '$void_context_result'";
+print "not " if $void_context_result ne "void"; say "ok # wantarray in void context returned '$void_context_result'";
 
 # Test wantarray in nested subroutine
-my @nested_list_context_result = sub { return sub { return wantarray ? "list" : "scalar" }->() }->();
+my @nested_list_context_result = sub { return sub { return wantarray_as_string(wantarray) }->() }->();
 print "not " if "@nested_list_context_result" ne "list"; say "ok # wantarray in nested list context returned '@nested_list_context_result'";
 
-### my $nested_scalar_context_result = sub { return sub { return wantarray ? "list" : "scalar" }->() }->();
-### print "not " if $nested_scalar_context_result ne "scalar"; say "ok # wantarray in nested scalar context returned '$nested_scalar_context_result'";
-### 
-### # Test wantarray with eval
-### my $eval_result = eval 'wantarray ? "list" : "scalar"';
-### print "not " if $eval_result ne "scalar"; say "ok # wantarray with eval returned '$eval_result'";
-### 
-### # Test wantarray with map
-### my @map_result = map { wantarray ? "list" : "scalar" } (1);
-### print "not " if "@map_result" ne "list"; say "ok # wantarray with map in list context returned '@map_result'";
-### 
-### my $map_scalar_result = scalar map { wantarray ? "list" : "scalar" } (1);
-### print "not " if $map_scalar_result ne "scalar"; say "ok # wantarray with map in scalar context returned '$map_scalar_result'";
-### 
-### # Test wantarray with grep
-### my @grep_result = grep { wantarray ? "list" : "scalar" } (1);
-### print "not " if "@grep_result" ne "list"; say "ok # wantarray with grep in list context returned '@grep_result'";
-### 
-### my $grep_scalar_result = scalar grep { wantarray ? "list" : "scalar" } (1);
-### print "not " if $grep_scalar_result ne "scalar"; say "ok # wantarray with grep in scalar context returned '$grep_scalar_result'";
-### 
+# TODO my $nested_scalar_context_result = sub { return sub { return wantarray_as_string(wantarray) }->() }->();
+# TODO print "not " if $nested_scalar_context_result ne "scalar"; say "ok # wantarray in nested scalar context returned '$nested_scalar_context_result'";
+
+# Test wantarray with eval
+my $eval_result = eval 'wantarray_as_string(wantarray)';
+print "not " if $eval_result ne "scalar"; say "ok # wantarray with eval returned '$eval_result'";
+
+my @eval_result = eval 'wantarray_as_string(wantarray)';
+print "not " if "@eval_result" ne "list"; say "ok # wantarray with eval returned '@eval_result'";
+
