1453357f183c76d2206a4d62b0fa59bb90cac0d7
Flavio S. Glock <fglock@gmail.com>
2024-09-05 14:26:30 +0200

    tr/// operator wip

--- src/main/java/org/perlonjava/codegen/EmitterVisitor.java
+++ src/main/java/org/perlonjava/codegen/EmitterVisitor.java
@@ -884,6 +884,7 @@ public class EmitterVisitor implements Visitor {
             case "matchRegex":
             case "quoteRegex":
             case "replaceRegex":
+            case "tr":
                 handleRegex(node);
                 break;
             default:
@@ -895,7 +896,31 @@ public class EmitterVisitor implements Visitor {
         ListNode operand = (ListNode) node.operand;
         EmitterVisitor scalarVisitor = this.with(RuntimeContextType.SCALAR);
         Node variable = null;
-        if (node.operator.equals("replaceRegex")) {
+        if (node.operator.equals("tr")) {
+            // static RuntimeTransliterate compile(RuntimeScalar search, RuntimeScalar replace, RuntimeScalar modifiers)
+            operand.elements.get(0).accept(scalarVisitor);
+            operand.elements.get(1).accept(scalarVisitor);
+            operand.elements.get(2).accept(scalarVisitor);
+            if (operand.elements.size() > 3) {
+                variable = operand.elements.get(3);
+            }
+            ctx.mv.visitMethodInsn(Opcodes.INVOKESTATIC,
+                    "org/perlonjava/runtime/RuntimeTransliterate", "compile",
+                    "(Lorg/perlonjava/runtime/RuntimeScalar;Lorg/perlonjava/runtime/RuntimeScalar;Lorg/perlonjava/runtime/RuntimeScalar;)Lorg/perlonjava/runtime/RuntimeTransliterate;", false);
+
+            // RuntimeScalar transliterate(RuntimeScalar originalString)
+            if (variable == null) {
+                // use `$_`
+                variable = new OperatorNode("$", new IdentifierNode("_", node.tokenIndex), node.tokenIndex);
+            }
+            variable.accept(scalarVisitor);
+            ctx.mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "org/perlonjava/runtime/RuntimeTransliterate", "transliterate", "(Lorg/perlonjava/runtime/RuntimeScalar;)Lorg/perlonjava/runtime/RuntimeScalar;", false);
+            if (ctx.contextType == RuntimeContextType.VOID) {
+                ctx.mv.visitInsn(Opcodes.POP);
+            }
+            return;
+
+        } else if (node.operator.equals("replaceRegex")) {
             // RuntimeBaseEntity replaceRegex(RuntimeScalar quotedRegex, RuntimeScalar string, RuntimeScalar replacement, int ctx)
             operand.elements.get(0).accept(scalarVisitor);
             operand.elements.get(1).accept(scalarVisitor);
