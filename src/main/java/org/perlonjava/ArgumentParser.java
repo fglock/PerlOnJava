c83203cea2cbcbe5d27cd2da474df954be72bf01
Flavio S. Glock <fglock@gmail.com>
2024-09-07 10:21:23 +0200

    add `@ARGV`

--- src/main/java/org/perlonjava/ArgumentParser.java
+++ src/main/java/org/perlonjava/ArgumentParser.java
@@ -1,5 +1,8 @@
 package org.perlonjava;
 
+import org.perlonjava.runtime.RuntimeArray;
+import org.perlonjava.runtime.RuntimeScalar;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -8,64 +11,71 @@ public class ArgumentParser {
     public static CompilerOptions parseArguments(String[] args) {
         CompilerOptions parsedArgs = new CompilerOptions();
         parsedArgs.code = null;
+        boolean readingArgv = false;
 
         for (int i = 0; i < args.length; i++) {
-            switch (args[i]) {
-                case "-e":
-                    if (i + 1 < args.length) {
-                        parsedArgs.code = args[i + 1];
-                        parsedArgs.fileName = "-e";
-                        i++;
-                    } else {
-                        System.err.println("Error: -e requires an argument");
-                        System.exit(1);
-                    }
-                    break;
-                case "--debug":
-                    parsedArgs.debugEnabled = true;
-                    break;
-                case "--tokenize":
-                    if (parsedArgs.parseOnly || parsedArgs.compileOnly) {
-                        System.err.println("Error: --tokenize cannot be combined with --parse or -c");
-                        System.exit(1);
-                    }
-                    parsedArgs.tokenizeOnly = true;
-                    break;
-                case "--parse":
-                    if (parsedArgs.tokenizeOnly || parsedArgs.compileOnly) {
-                        System.err.println("Error: --parse cannot be combined with --tokenize or -c");
-                        System.exit(1);
-                    }
-                    parsedArgs.parseOnly = true;
-                    break;
-                case "--disassemble":
-                    if (parsedArgs.tokenizeOnly || parsedArgs.parseOnly) {
-                        System.err.println("Error: --disassemble cannot be combined with --tokenize or --parse");
-                        System.exit(1);
-                    }
-                    parsedArgs.disassembleEnabled = true;
-                    break;
-                case "-c":
-                    if (parsedArgs.tokenizeOnly || parsedArgs.parseOnly) {
-                        System.err.println("Error: -c cannot be combined with --tokenize or --parse");
-                        System.exit(1);
-                    }
-                    parsedArgs.compileOnly = true;
-                    break;
-                case "-h":
-                case "--help":
-                    printHelp();
-                    System.exit(0);
-                    break;
-                default:
-                    parsedArgs.fileName = args[i];
-                    try {
-                        parsedArgs.code = new String(Files.readAllBytes(Paths.get(parsedArgs.fileName)));
-                    } catch (IOException e) {
-                        System.err.println("Error: Unable to read file " + parsedArgs.fileName);
-                        System.exit(1);
-                    }
-                    break;
+            if (readingArgv) {
+                parsedArgs.argumentList.push(new RuntimeScalar(args[i]));
+            } else {
+                switch (args[i]) {
+                    case "-e":
+                        if (i + 1 < args.length) {
+                            parsedArgs.code = args[i + 1];
+                            parsedArgs.fileName = "-e";
+                            i++;
+                            readingArgv = true;
+                        } else {
+                            System.err.println("Error: -e requires an argument");
+                            System.exit(1);
+                        }
+                        break;
+                    case "--debug":
+                        parsedArgs.debugEnabled = true;
+                        break;
+                    case "--tokenize":
+                        if (parsedArgs.parseOnly || parsedArgs.compileOnly) {
+                            System.err.println("Error: --tokenize cannot be combined with --parse or -c");
+                            System.exit(1);
+                        }
+                        parsedArgs.tokenizeOnly = true;
+                        break;
+                    case "--parse":
+                        if (parsedArgs.tokenizeOnly || parsedArgs.compileOnly) {
+                            System.err.println("Error: --parse cannot be combined with --tokenize or -c");
+                            System.exit(1);
+                        }
+                        parsedArgs.parseOnly = true;
+                        break;
+                    case "--disassemble":
+                        if (parsedArgs.tokenizeOnly || parsedArgs.parseOnly) {
+                            System.err.println("Error: --disassemble cannot be combined with --tokenize or --parse");
+                            System.exit(1);
+                        }
+                        parsedArgs.disassembleEnabled = true;
+                        break;
+                    case "-c":
+                        if (parsedArgs.tokenizeOnly || parsedArgs.parseOnly) {
+                            System.err.println("Error: -c cannot be combined with --tokenize or --parse");
+                            System.exit(1);
+                        }
+                        parsedArgs.compileOnly = true;
+                        break;
+                    case "-h":
+                    case "--help":
+                        printHelp();
+                        System.exit(0);
+                        break;
+                    default:
+                        parsedArgs.fileName = args[i];
+                        try {
+                            parsedArgs.code = new String(Files.readAllBytes(Paths.get(parsedArgs.fileName)));
+                            readingArgv = true;
+                        } catch (IOException e) {
+                            System.err.println("Error: Unable to read file " + parsedArgs.fileName);
+                            System.exit(1);
+                        }
+                        break;
+                }
             }
         }
 
@@ -73,7 +83,7 @@ public class ArgumentParser {
     }
 
     private static void printHelp() {
-        System.out.println("Usage: java -cp <classpath> org.perlonjava.Main [options] [file]");
+        System.out.println("Usage: java -cp <classpath> org.perlonjava.Main [options] [file] [args]");
         System.out.println("Options:");
         System.out.println("  -e <code>       Specifies the code to be processed.");
         System.out.println("  --debug         Enables debugging mode.");
@@ -108,6 +118,7 @@ public class ArgumentParser {
         public boolean compileOnly = false;
         public String code = null;
         public String fileName = null;
+        public RuntimeArray argumentList = new RuntimeArray();
 
         @Override
         public CompilerOptions clone() {
