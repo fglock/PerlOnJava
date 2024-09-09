ad32b55bcdd70e07a4f6372e6384bb059a673fc8
Flavio S. Glock <fglock@gmail.com>
2024-09-09 15:19:27 +0200

    refactoring

--- src/main/java/org/perlonjava/codegen/EmitterVisitor.java
+++ src/main/java/org/perlonjava/codegen/EmitterVisitor.java
@@ -138,7 +138,7 @@ public class EmitterVisitor implements Visitor {
                 handleOrEqualOperator(node, Opcodes.IFEQ);
                 return;
             case "=":
-                handleAssignOperator(node);
+                EmitVariable.handleAssignOperator(this, node);
                 return;
             case ".":
                 handleConcatOperator(node, scalarVisitor);
@@ -806,7 +806,7 @@ public class EmitterVisitor implements Visitor {
             case "%":
             case "*":
             case "&":
-                handleVariableOperator(node, operator);
+                EmitVariable.handleVariableOperator(this, node, operator);
                 break;
             case "keys":
             case "values":
@@ -817,7 +817,7 @@ public class EmitterVisitor implements Visitor {
                 break;
             case "our":
             case "my":
-                handleMyOperator(node, operator);
+                EmitVariable.handleMyOperator(this, node, operator);
                 break;
             case "return":
                 handleReturnOperator(node);
@@ -1051,257 +1051,6 @@ public class EmitterVisitor implements Visitor {
         if (ctx.contextType == RuntimeContextType.VOID) {
             ctx.mv.visitInsn(Opcodes.POP);
         }
-    }
-
-    private void fetchGlobalVariable(boolean createIfNotExists, String sigil, String varName, int tokenIndex) {
-
-        String var = GlobalContext.normalizeVariableName(varName, ctx.symbolTable.getCurrentPackage());
-        ctx.logDebug("GETVAR lookup global " + sigil + varName + " normalized to " + var + " createIfNotExists:" + createIfNotExists);
-
-        if (sigil.equals("$") && (createIfNotExists || GlobalContext.existsGlobalVariable(var))) {
-            // fetch a global variable
-            ctx.mv.visitLdcInsn(var);
-            ctx.mv.visitMethodInsn(
-                    Opcodes.INVOKESTATIC,
-                    "org/perlonjava/runtime/GlobalContext",
-                    "getGlobalVariable",
-                    "(Ljava/lang/String;)Lorg/perlonjava/runtime/RuntimeScalar;",
-                    false);
-        } else if (sigil.equals("@") && (createIfNotExists || GlobalContext.existsGlobalArray(var))) {
-            // fetch a global variable
-            ctx.mv.visitLdcInsn(var);
-            ctx.mv.visitMethodInsn(
-                    Opcodes.INVOKESTATIC,
-                    "org/perlonjava/runtime/GlobalContext",
-                    "getGlobalArray",
-                    "(Ljava/lang/String;)Lorg/perlonjava/runtime/RuntimeArray;",
-                    false);
-        } else if (sigil.equals("%") && (createIfNotExists || GlobalContext.existsGlobalHash(var))) {
-            // fetch a global variable
-            ctx.mv.visitLdcInsn(var);
-            ctx.mv.visitMethodInsn(
-                    Opcodes.INVOKESTATIC,
-                    "org/perlonjava/runtime/GlobalContext",
-                    "getGlobalHash",
-                    "(Ljava/lang/String;)Lorg/perlonjava/runtime/RuntimeHash;",
-                    false);
-        } else {
-            // variable not found
-            System.err.println(
-                    ctx.errorUtil.errorMessage(tokenIndex,
-                            "Warning: Global symbol \""
-                                    + sigil + varName
-                                    + "\" requires explicit package name (did you forget to declare \"my "
-                                    + sigil + varName
-                                    + "\"?)"));
-        }
-    }
-
-    private void handleVariableOperator(OperatorNode node, String sigil) {
-        if (ctx.contextType == RuntimeContextType.VOID) {
-            return;
-        }
-        MethodVisitor mv = ctx.mv;
-        if (node.operand instanceof IdentifierNode) { // $a @a %a
-            String name = ((IdentifierNode) node.operand).name;
-            ctx.logDebug("GETVAR " + sigil + name);
-
-            if (sigil.equals("*")) {
-                // typeglob
-                String fullName = GlobalContext.normalizeVariableName(name, ctx.symbolTable.getCurrentPackage());
-                mv.visitTypeInsn(Opcodes.NEW, "org/perlonjava/runtime/RuntimeGlob");
-                mv.visitInsn(Opcodes.DUP);
-                mv.visitLdcInsn(fullName); // emit string
-                mv.visitMethodInsn(
-                        Opcodes.INVOKESPECIAL,
-                        "org/perlonjava/runtime/RuntimeGlob",
-                        "<init>",
-                        "(Ljava/lang/String;)V",
-                        false); // Call new RuntimeGlob(String)
-                return;
-            }
-
-            if (sigil.equals("&")) {
-                // Code
-                String fullName = GlobalContext.normalizeVariableName(name, ctx.symbolTable.getCurrentPackage());
-                mv.visitLdcInsn(fullName); // emit string
-                ctx.mv.visitMethodInsn(
-                        Opcodes.INVOKESTATIC,
-                        "org/perlonjava/runtime/GlobalContext",
-                        "getGlobalCodeRef",
-                        "(Ljava/lang/String;)Lorg/perlonjava/runtime/RuntimeScalar;",
-                        false);
-                return;
-            }
-
-            int varIndex = ctx.symbolTable.getVariableIndex(sigil + name);
-            if (varIndex == -1) {
-                // not a declared `my` or `our` variable
-                // Fetch a global variable.
-                // Autovivify if the name is fully qualified, or if it is a regex variable like `$1`
-                // TODO special variables: `$,` `$$`
-                boolean createIfNotExists = name.contains("::") || isInteger(name);
-                fetchGlobalVariable(createIfNotExists, sigil, name, node.getIndex());
-            } else {
-                // retrieve the `my` or `our` variable from local vars
-                mv.visitVarInsn(Opcodes.ALOAD, varIndex);
-            }
-            if (ctx.contextType == RuntimeContextType.SCALAR && !sigil.equals("$")) {
-                // scalar context: transform the value in the stack to scalar
-                mv.visitMethodInsn(Opcodes.INVOKEINTERFACE, "org/perlonjava/runtime/RuntimeDataProvider", "scalar", "()Lorg/perlonjava/runtime/RuntimeScalar;", true);
-            }
-            ctx.logDebug("GETVAR end " + varIndex);
-            return;
-        }
-        switch (sigil) {
-            case "@":
-                // `@$a`
-                ctx.logDebug("GETVAR `@$a`");
-                node.operand.accept(this.with(RuntimeContextType.LIST));
-                mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "org/perlonjava/runtime/RuntimeScalar", "arrayDeref", "()Lorg/perlonjava/runtime/RuntimeArray;", false);
-                return;
-            case "%":
-                // `%$a`
-                ctx.logDebug("GETVAR `%$a`");
-                node.operand.accept(this.with(RuntimeContextType.LIST));
-                mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "org/perlonjava/runtime/RuntimeScalar", "hashDeref", "()Lorg/perlonjava/runtime/RuntimeHash;", false);
-                return;
-            case "$":
-                // `$$a`
-                ctx.logDebug("GETVAR `$$a`");
-                node.operand.accept(this.with(RuntimeContextType.SCALAR));
-                mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "org/perlonjava/runtime/RuntimeScalar", "scalarDeref", "()Lorg/perlonjava/runtime/RuntimeScalar;", false);
-                return;
-        }
-
-        // TODO ${a} ${[ 123 ]}
-        throw new PerlCompilerException(node.tokenIndex, "Not implemented: " + sigil, ctx.errorUtil);
-    }
-
-    private void handleAssignOperator(BinaryOperatorNode node) {
-        ctx.logDebug("SET " + node);
-        MethodVisitor mv = ctx.mv;
-        // Determine the assign type based on the left side.
-        // Inspect the AST and get the L-value context: SCALAR or LIST
-        int lvalueContext = LValueVisitor.getContext(node);
-        ctx.logDebug("SET Lvalue context: " + lvalueContext);
-        // Execute the right side first: assignment is right-associative
-        switch (lvalueContext) {
-            case RuntimeContextType.SCALAR:
-                ctx.logDebug("SET right side scalar");
-                node.right.accept(this.with(RuntimeContextType.SCALAR));   // emit the value
-                node.left.accept(this.with(RuntimeContextType.SCALAR));   // emit the variable
-
-                boolean isGlob = false;
-                String leftDescriptor = "org/perlonjava/runtime/RuntimeScalar";
-                if (node.left instanceof OperatorNode && ((OperatorNode) node.left).operator.equals("*")) {
-                    leftDescriptor = "org/perlonjava/runtime/RuntimeGlob";
-                    isGlob = true;
-                }
-                String rightDescriptor = "(Lorg/perlonjava/runtime/RuntimeScalar;)Lorg/perlonjava/runtime/RuntimeScalar;";
-                if (node.right instanceof OperatorNode && ((OperatorNode) node.right).operator.equals("*")) {
-                    rightDescriptor = "(Lorg/perlonjava/runtime/RuntimeGlob;)Lorg/perlonjava/runtime/RuntimeScalar;";
-                    isGlob = true;
-                }
-                if (isGlob) {
-                    mv.visitInsn(Opcodes.SWAP); // move the target first
-                    mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, leftDescriptor, "set", rightDescriptor, false);
-                } else {
-                    mv.visitMethodInsn(Opcodes.INVOKEINTERFACE, "org/perlonjava/runtime/RuntimeDataProvider", "addToScalar", "(Lorg/perlonjava/runtime/RuntimeScalar;)Lorg/perlonjava/runtime/RuntimeScalar;", true);
-                }
-                break;
-            case RuntimeContextType.LIST:
-                ctx.logDebug("SET right side list");
-                Node nodeRight = node.right;
-                // make sure the right node is a ListNode
-                if (!(nodeRight instanceof ListNode)) {
-                    List<Node> elements = new ArrayList<>();
-                    elements.add(nodeRight);
-                    nodeRight = new ListNode(elements, node.tokenIndex);
-                }
-                nodeRight.accept(this.with(RuntimeContextType.LIST));   // emit the value
-                node.left.accept(this.with(RuntimeContextType.LIST));   // emit the variable
-                mv.visitInsn(Opcodes.SWAP); // move the target first
-                mv.visitMethodInsn(Opcodes.INVOKEINTERFACE, "org/perlonjava/runtime/RuntimeDataProvider", "set", "(Lorg/perlonjava/runtime/RuntimeList;)Lorg/perlonjava/runtime/RuntimeArray;", true);
-                break;
-            default:
-                throw new IllegalArgumentException("Unsupported assignment context: " + lvalueContext);
-        }
-        if (ctx.contextType == RuntimeContextType.VOID) {
-            // Remove the value from the stack
-            mv.visitInsn(Opcodes.POP);
-        }
-        ctx.logDebug("SET end");
-    }
-
-    private void handleMyOperator(OperatorNode node, String operator) {
-        if (node.operand instanceof ListNode) { // my ($a, $b)  our ($a, $b)
-            // process each item of the list; then returns the list
-            ListNode listNode = (ListNode) node.operand;
-            for (Node element : listNode.elements) {
-                if (element instanceof OperatorNode && "undef".equals(((OperatorNode) element).operator)) {
-                    continue; // skip "undef"
-                }
-                OperatorNode myNode = new OperatorNode(operator, element, listNode.tokenIndex);
-                myNode.accept(this.with(RuntimeContextType.VOID));
-            }
-            if (ctx.contextType != RuntimeContextType.VOID) {
-                listNode.accept(this);
-            }
-            return;
-        } else if (node.operand instanceof OperatorNode) { //  [my our] followed by [$ @ %]
-            OperatorNode sigilNode = (OperatorNode) node.operand;
-            String sigil = sigilNode.operator;
-            if ("$@%".contains(sigil)) {
-                Node identifierNode = sigilNode.operand;
-                if (identifierNode instanceof IdentifierNode) { // my $a
-                    String name = ((IdentifierNode) identifierNode).name;
-                    String var = sigil + name;
-                    ctx.logDebug("MY " + operator + " " + sigil + name);
-                    if (ctx.symbolTable.getVariableIndexInCurrentScope(var) != -1) {
-                        System.err.println(
-                                ctx.errorUtil.errorMessage(node.getIndex(),
-                                        "Warning: \"" + operator + "\" variable "
-                                                + var
-                                                + " masks earlier declaration in same ctx.symbolTable"));
-                    }
-                    int varIndex = ctx.symbolTable.addVariable(var);
-                    // TODO optimization - SETVAR+MY can be combined
-
-                    // Determine the class name based on the sigil
-                    String className = EmitterMethodCreator.getVariableClassName(sigil);
-
-                    if (operator.equals("my")) {
-                        // "my":
-                        // Create a new instance of the determined class
-                        ctx.mv.visitTypeInsn(Opcodes.NEW, className);
-                        ctx.mv.visitInsn(Opcodes.DUP);
-                        ctx.mv.visitMethodInsn(
-                                Opcodes.INVOKESPECIAL,
-                                className,
-                                "<init>",
-                                "()V",
-                                false);
-                    } else {
-                        // "our":
-                        // Create and fetch a global variable
-                        fetchGlobalVariable(true, sigil, name, node.getIndex());
-                    }
-                    if (ctx.contextType != RuntimeContextType.VOID) {
-                        ctx.mv.visitInsn(Opcodes.DUP);
-                    }
-                    // Store in a JVM local variable
-                    ctx.mv.visitVarInsn(Opcodes.ASTORE, varIndex);
-                    if (ctx.contextType == RuntimeContextType.SCALAR && !sigil.equals("$")) {
-                        // scalar context: transform the value in the stack to scalar
-                        ctx.mv.visitMethodInsn(Opcodes.INVOKEINTERFACE, "org/perlonjava/runtime/RuntimeDataProvider", "scalar", "()Lorg/perlonjava/runtime/RuntimeScalar;", true);
-                    }
-                    return;
-                }
-            }
-        }
-        throw new PerlCompilerException(
-                node.tokenIndex, "Not implemented: " + node.operator, ctx.errorUtil);
     }
 
     private void handleReturnOperator(OperatorNode node) {
