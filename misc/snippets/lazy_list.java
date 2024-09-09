025da2e0e5709fcfa300febbd0abf5c25945cff3
fglock <fglock@gmail.com>
2024-09-09 20:03:02 +0200

    add snippet

---
+++ misc/snippets/lazy_list.java
@@ -0,0 +1,93 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class RuntimeList extends RuntimeBaseEntity implements RuntimeDataProvider {
+    private List<RuntimeBaseEntity> elements;
+    private boolean isLazy = false;
+    private Iterator<RuntimeScalar> lazyIterator; // Store the iterator for lazy generation
+
+    // Constructor
+    public RuntimeList() {
+        this.elements = new ArrayList<>();
+    }
+
+    public RuntimeList(RuntimeScalar value) {
+        this.elements = new ArrayList<>();
+        this.elements.add(value);
+    }
+
+    public RuntimeList(RuntimeList value) {
+        this.elements = value.elements;
+    }
+
+    public RuntimeList(RuntimeArray value) {
+        this.elements = value.elements;
+    }
+
+    public RuntimeList(RuntimeHash value) {
+        this.elements = value.entryArray().elements;
+    }
+
+    // Method to generate a lazy RuntimeList
+    public static RuntimeList generateLazyList(RuntimeScalar startValue, RuntimeScalar endValue) {
+        RuntimeList list = new RuntimeList();
+        list.isLazy = true;
+        list.lazyIterator = createLazyIterator(startValue, endValue);
+        return list;
+    }
+
+    // Create a lazy iterator for RuntimeScalar objects
+    private static Iterator<RuntimeScalar> createLazyIterator(RuntimeScalar startValue, RuntimeScalar endValue) {
+        int start = startValue.getInt();
+        int end = endValue.getInt();
+
+        return new Iterator<RuntimeScalar>() {
+            private int current = start;
+
+            @Override
+            public boolean hasNext() {
+                return current <= end;
+            }
+
+            @Override
+            public RuntimeScalar next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return new RuntimeScalar(current++);
+            }
+        };
+    }
+
+    // Method to materialize the list if it's in a lazy state
+    private void materializeIfNeeded() {
+        if (isLazy) {
+            while (lazyIterator.hasNext()) {
+                this.elements.add(lazyIterator.next());
+            }
+            isLazy = false; // The list has been materialized, no longer lazy
+        }
+    }
+
+    // Override methods that require the list elements
+    public int size() {
+        materializeIfNeeded();
+        return elements.size();
+    }
+
+    public RuntimeBaseEntity get(int index) {
+        materializeIfNeeded();
+        return elements.get(index);
+    }
+
+    public void add(RuntimeBaseEntity value) {
+        materializeIfNeeded();
+        this.elements.add(value);
+    }
+
+    // Other methods that rely on accessing elements will also call materializeIfNeeded()
+    ...
+}
+
