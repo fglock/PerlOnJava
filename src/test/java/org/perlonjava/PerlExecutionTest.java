5ec403f849ae5c8b2095b7b87a1f44650c7aa742
Flavio S. Glock <fglock@gmail.com>
2024-09-24 16:52:11 +0200

    make sure Perl test output is redirected (testing for "not ok") cleanup

--- src/test/java/org/perlonjava/PerlExecutionTest.java
+++ src/test/java/org/perlonjava/PerlExecutionTest.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.perlonjava.scriptengine.PerlLanguageProvider;
 import org.perlonjava.runtime.RuntimeIO;
+import org.perlonjava.scriptengine.PerlLanguageProvider;
 
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
