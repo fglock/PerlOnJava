5ba0a862e1c0ffa87e09e4c71b552e86c640b652
Flavio S. Glock <fglock@gmail.com>
2024-10-10 14:14:59 +0200

    `<>` wip

--- src/main/java/org/perlonjava/runtime/DiamondIO.java
+++ src/main/java/org/perlonjava/runtime/DiamondIO.java
@@ -1,52 +1,37 @@
 package org.perlonjava.runtime;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 import static org.perlonjava.runtime.GlobalContext.*;
 import static org.perlonjava.runtime.RuntimeScalarCache.scalarUndef;
 
-public class DiamondIO extends RuntimeIO {
+public class DiamondIO {
 
-    private BufferedReader currentReader;
-    private boolean eofReached = false;
+    static RuntimeIO currentReader;
+    static boolean eofReached = false;
 
-    /**
-     * Constructor for DiamondIO.
-     */
-    public DiamondIO() {
-        super();
-    }
-
-    public RuntimeScalar readline() {
+    static RuntimeScalar readline() {
         if (eofReached) {
             return scalarUndef;
         }
 
-        try {
-            while (currentReader == null || isEOF()) {
-                if (!openNextFile()) {
-                    eofReached = true;
-                    return scalarUndef;
-                }
+        while (currentReader == null || !currentReader.eof().getBoolean()) {
+            if (!openNextFile()) {
+                eofReached = true;
+                return scalarUndef;
             }
-
-            String line = currentReader.readLine();
-            if (line == null) {
-                return readline();
-            }
-            return new RuntimeScalar(line);
-        } catch (IOException e) {
-            getGlobalVariable("main::!").set("File operation failed: " + e.getMessage());
-            return scalarUndef;
         }
+
+        RuntimeScalar line = currentReader.readline();
+        if (line.type == RuntimeScalarType.UNDEF) {
+            return readline();
+        }
+        return line;
     }
 
-    private boolean openNextFile() {
-        closeCurrentReader();
+    private static boolean openNextFile() {
+        if (currentReader != null) {
+            currentReader.close();
+            currentReader = null;
+        }
         RuntimeScalar fileName = getGlobalArray("main::ARGV").shift();
 
         if (fileName.type == RuntimeScalarType.UNDEF) {
@@ -56,35 +41,11 @@ public class DiamondIO extends RuntimeIO {
         // Set the current filename in the global $main::ARGV variable
         getGlobalVariable("main::ARGV").set(fileName);
 
-        try {
-            if ("-".equals(fileName.toString())) {
-                currentReader = new BufferedReader(new InputStreamReader(System.in));
-            } else {
-                currentReader = Files.newBufferedReader(Paths.get(fileName.toString()));
-            }
-        } catch (IOException e) {
-            getGlobalVariable("main::!").set("Failed to open file: " + fileName + ": " + e.getMessage());
-            return false;
-        }
-        return true;
-    }
+        currentReader = RuntimeIO.open(fileName.toString());
 
-    private void closeCurrentReader() {
-        if (currentReader != null) {
-            try {
-                currentReader.close();
-            } catch (IOException e) {
-                getGlobalVariable("main::!").set("Failed to close file: " + e.getMessage());
-            }
-            currentReader = null;
-        }
-    }
+        // Set the current handle in the global main::ARGV handle
+        getGlobalIO("main::ARGV").set(currentReader);
 
-    private boolean isEOF() {
-        try {
-            return currentReader != null && !currentReader.ready();
-        } catch (IOException e) {
-            return true;
-        }
+        return currentReader != null;
     }
 }
