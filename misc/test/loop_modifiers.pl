35691fc9ca7d9c6cadefad6c43af4f44b2dc68b2
Flavio S. Glock <fglock@gmail.com>
2024-10-11 11:02:38 +0200

    tests

--- misc/test/loop_modifiers.pl
+++ misc/test/loop_modifiers.pl
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 
 ###################
-# Perl `next` Tests with `do ... while ...`
+# `next` Tests with `do ... while ...`
 
 my $next_do_while_count = 0;
 eval q{
@@ -18,10 +18,10 @@ my $error_message = $@ // "No error";
 print "not "
   if !($error_message
     && $error_message =~ /Can't "next" outside a loop block/ );
-say "ok # Correct error message for `next` outside a loop";
+say "ok # error message for `next` outside a loop";
 
 ###################
-# Perl `next` Tests with `do ... while ...` with an outer loop
+# `next` Tests with `do ... while ...` with an outer loop
 
 $next_do_while_count = 0;
 {
@@ -31,14 +31,31 @@ $next_do_while_count = 0;
     } while ( $next_do_while_count < 4 );
 }
 print "not " if $next_do_while_count != 2;
-say "ok # Correct `next` outside a loop";
+say "ok # `next` outside a loop";
 
 ###################
-# Perl `next` Tests with `for` modifier
+# `next` Tests with `for` modifier
 
 my $next_for_count = 0;
 $next_for_count++ for 1 .. 3;
 $next_for_count == 2 && next for 1 .. 3;    # Skip when count is 2
 print "not " if $next_for_count != 3;
 say "ok # `next` in `for` modifier";
+
+
+###################
+# `while` loop
+
+my $while_mod_count = 0;
+$while_mod_count++ while 0;     # never executes
+print "not " if $while_mod_count != 0;
+say "ok # `while` loop with statement modifier";
+
+###################
+# `do-while` loop
+
+$while_mod_count = 0;
+do { $while_mod_count++ } while 0;  # executes once
+print "not " if $while_mod_count != 1;
+say "ok # `while` loop with statement modifier";
 
