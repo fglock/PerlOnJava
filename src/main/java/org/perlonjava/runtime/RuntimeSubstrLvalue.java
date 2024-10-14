ef1fc9a27776297db3f2af9d8d2fa24a69ab7b3a
Flavio S. Glock <fglock@gmail.com>
2024-10-14 15:47:44 +0200

    lvalue substr wip

--- src/main/java/org/perlonjava/runtime/RuntimeSubstrLvalue.java
+++ src/main/java/org/perlonjava/runtime/RuntimeSubstrLvalue.java
@@ -1,70 +1,46 @@
 package org.perlonjava.runtime;
 
 public class RuntimeSubstrLvalue extends RuntimeBaseProxy {
-    private final RuntimeScalar parent;
-    private int offset;
+    private final int offset;
     private final int length;
-    private final boolean isNegativeOffset;
 
-    public RuntimeSubstrLvalue(RuntimeScalar parent, int offset, int length) {
-        super();
-        this.parent = parent;
+    public RuntimeSubstrLvalue(RuntimeScalar parent, String str, int offset, int length) {
+        this.lvalue = parent;
         this.offset = offset;
         this.length = length;
-        this.isNegativeOffset = offset < 0;
+
+        this.type = RuntimeScalarType.STRING;
+        this.value = str;
     }
 
     @Override
     void vivify() {
-        if (lvalue == null) {
-            String parentValue = parent.toString();
-            int actualOffset = isNegativeOffset ? parentValue.length() + offset : offset;
-            int endIndex = Math.min(actualOffset + length, parentValue.length());
+            String parentValue = lvalue.toString();
+            String newValue = this.toString();
+            int actualOffset = offset < 0 ? parentValue.length() + offset : offset;
 
             if (actualOffset < 0) {
                 actualOffset = 0;
             }
 
-            if (actualOffset < parentValue.length()) {
-                String substring = parentValue.substring(actualOffset, endIndex);
-                lvalue = new RuntimeScalar(substring);
+            StringBuilder updatedValue = new StringBuilder(parentValue);
+
+            if (actualOffset >= parentValue.length()) {
+                updatedValue.append(" ".repeat(actualOffset - parentValue.length()));
+                updatedValue.append(newValue);
             } else {
-                lvalue = new RuntimeScalar();
+                int endIndex = Math.min(actualOffset + length, parentValue.length());
+                updatedValue.replace(actualOffset, endIndex, newValue);
             }
-        }
+
+            lvalue.set(new RuntimeScalar(updatedValue.toString()));
     }
 
     @Override
     public RuntimeScalar set(RuntimeScalar value) {
+        this.type = value.type;
+        this.value = value.value;
         vivify();
-        RuntimeScalar result = super.set(value);
-        updateParent();
-        return result;
-    }
-
-    private void updateParent() {
-        String parentValue = parent.toString();
-        String newValue = lvalue.toString();
-        int actualOffset = isNegativeOffset ? parentValue.length() + offset : offset;
-
-        if (actualOffset < 0) {
-            actualOffset = 0;
-        }
-
-        StringBuilder updatedValue = new StringBuilder(parentValue);
-
-        if (actualOffset >= parentValue.length()) {
-            updatedValue.append(" ".repeat(actualOffset - parentValue.length()));
-            updatedValue.append(newValue);
-        } else {
-            int endIndex = Math.min(actualOffset + length, parentValue.length());
-            updatedValue.replace(actualOffset, endIndex, newValue);
-        }
-
-        parent.set(new RuntimeScalar(updatedValue.toString()));
-
-        if (isNegativeOffset) {
-            offset = parent.toString().length() + offset;
-        }
+        return this;
     }
 }
