e90ba8c9e01b84d10545aaeee0131c09d21e372f
Flavio S. Glock <fglock@gmail.com>
2024-10-18 10:32:03 +0200

    implement `@+` and `@-` as a dynamic view over a Matcher object

--- src/main/java/org/perlonjava/runtime/GlobalContext.java
+++ src/main/java/org/perlonjava/runtime/GlobalContext.java
@@ -50,8 +50,8 @@ public class GlobalContext {
         getGlobalVariable("main::0").set(compilerOptions.fileName);
 
         // Initialize arrays
-        getGlobalArray("main::+");  // regex @+
-        getGlobalArray("main::-");  // regex @-
+        getGlobalArray("main::+").elements = new MatcherViewArray(MatcherViewArray.Mode.END);  // regex @+
+        getGlobalArray("main::-").elements = new MatcherViewArray(MatcherViewArray.Mode.START);  // regex @-
 
         // Initialize %ENV
         Map<String, RuntimeScalar> env = getGlobalHash("main::ENV").elements;
