4b4bf94f1c82b1c48655b3b84f5bc0ee67176b2a
Flavio S. Glock <fglock@gmail.com>
2024-10-23 14:16:19 +0200

    state variables wip: tests

--- misc/test/state.pl
+++ misc/test/state.pl
@@ -2,8 +2,8 @@
 
 use strict;
 use warnings;
+use Test::More tests => 9;
 use feature 'state';
-use Test::More tests => 4;
 
 # Function using a state variable
 sub counter {
@@ -17,12 +17,32 @@ is(counter(), 1, 'First call to counter() should return 1');
 is(counter(), 2, 'Second call to counter() should return 2');
 is(counter(), 3, 'Third call to counter() should return 3');
 
-# Test that a new instance of the function does not reset the state variable
-sub another_counter {
-    state $count = 10;  # Initialize state variable with a different value
-    $count++;
-    return $count;
+# Function to create a new counter using an anonymous subroutine
+sub create_counter {
+    return sub {
+        state $x = 0;  # Each instance has its own state variable
+        return ++$x;
+    };
 }
 
-is(another_counter(), 11, 'First call to another_counter() should return 11');
+# Create two separate counters
+my $counter1 = create_counter();
+my $counter2 = create_counter();
+
+# Test that each counter maintains its own state
+is($counter1->(), 1, 'First call to counter1 should return 1');
+is($counter1->(), 2, 'Second call to counter1 should return 2');
+is($counter2->(), 1, 'First call to counter2 should return 1');
+is($counter2->(), 2, 'Second call to counter2 should return 2');
+
+# Test that state variable initialization only occurs once
+sub single_init {
+    state $y = 42;
+    return $y++;
+}
+
+is(single_init(), 42, 'First call to single_init should return 42');
+is(single_init(), 43, 'Second call to single_init should return 43');
+
+done_testing();
 
