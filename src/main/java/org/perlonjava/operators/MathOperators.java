f392bf671fad15c9e81926ec800f6f45257b0a98
Flavio S. Glock <fglock@gmail.com>
2024-10-25 14:05:44 +0200

    refactoring: reorganize operators

--- src/main/java/org/perlonjava/operators/MathOperators.java
+++ src/main/java/org/perlonjava/operators/MathOperators.java
@@ -10,7 +10,7 @@ import static org.perlonjava.runtime.RuntimeScalarCache.getScalarInt;
 /**
  * Provides basic arithmetic operations for RuntimeScalar objects.
  * This class includes methods for addition, subtraction, multiplication,
- * division, and modulus operations.
+ * division, modulus, and various mathematical functions.
  */
 public class MathOperators {
 
@@ -22,9 +22,11 @@ public class MathOperators {
      * @return A new RuntimeScalar representing the sum.
      */
     public static RuntimeScalar add(RuntimeScalar arg1, int arg2) {
+        // Convert string type to number if necessary
         if (arg1.type == RuntimeScalarType.STRING) {
             arg1 = NumberParser.parseNumber(arg1);
         }
+        // Perform addition based on the type of RuntimeScalar
         if (arg1.type == RuntimeScalarType.DOUBLE) {
             return new RuntimeScalar(arg1.getDouble() + arg2);
         } else {
@@ -41,12 +43,14 @@ public class MathOperators {
      */
     public static RuntimeScalar add(RuntimeScalar runtimeScalar, RuntimeScalar arg2) {
         RuntimeScalar arg1 = runtimeScalar;
+        // Convert string type to number if necessary
         if (arg1.type == RuntimeScalarType.STRING) {
             arg1 = NumberParser.parseNumber(arg1);
         }
         if (arg2.type == RuntimeScalarType.STRING) {
             arg2 = NumberParser.parseNumber(arg2);
         }
+        // Perform addition based on the type of RuntimeScalar
         if (arg1.type == RuntimeScalarType.DOUBLE || arg2.type == RuntimeScalarType.DOUBLE) {
             return new RuntimeScalar(arg1.getDouble() + arg2.getDouble());
         } else {
@@ -62,9 +66,11 @@ public class MathOperators {
      * @return A new RuntimeScalar representing the difference.
      */
     public static RuntimeScalar subtract(RuntimeScalar arg1, int arg2) {
+        // Convert string type to number if necessary
         if (arg1.type == RuntimeScalarType.STRING) {
             arg1 = NumberParser.parseNumber(arg1);
         }
+        // Perform subtraction based on the type of RuntimeScalar
         if (arg1.type == RuntimeScalarType.DOUBLE) {
             return new RuntimeScalar(arg1.getDouble() - arg2);
         } else {
@@ -80,12 +86,14 @@ public class MathOperators {
      * @return A new RuntimeScalar representing the difference.
      */
     public static RuntimeScalar subtract(RuntimeScalar arg1, RuntimeScalar arg2) {
+        // Convert string type to number if necessary
         if (arg1.type == RuntimeScalarType.STRING) {
             arg1 = NumberParser.parseNumber(arg1);
         }
         if (arg2.type == RuntimeScalarType.STRING) {
             arg2 = NumberParser.parseNumber(arg2);
         }
+        // Perform subtraction based on the type of RuntimeScalar
         if (arg1.type == RuntimeScalarType.DOUBLE || arg2.type == RuntimeScalarType.DOUBLE) {
             return new RuntimeScalar(arg1.getDouble() - arg2.getDouble());
         } else {
@@ -101,12 +109,14 @@ public class MathOperators {
      * @return A new RuntimeScalar representing the product.
      */
     public static RuntimeScalar multiply(RuntimeScalar arg1, RuntimeScalar arg2) {
+        // Convert string type to number if necessary
         if (arg1.type == RuntimeScalarType.STRING) {
             arg1 = NumberParser.parseNumber(arg1);
         }
         if (arg2.type == RuntimeScalarType.STRING) {
             arg2 = NumberParser.parseNumber(arg2);
         }
+        // Perform multiplication based on the type of RuntimeScalar
         if (arg1.type == RuntimeScalarType.DOUBLE || arg2.type == RuntimeScalarType.DOUBLE) {
             return new RuntimeScalar(arg1.getDouble() * arg2.getDouble());
         } else {
@@ -123,6 +133,7 @@ public class MathOperators {
      * @throws PerlCompilerException if division by zero occurs.
      */
     public static RuntimeScalar divide(RuntimeScalar arg1, RuntimeScalar arg2) {
+        // Convert string type to number if necessary
         if (arg1.type == RuntimeScalarType.STRING) {
             arg1 = NumberParser.parseNumber(arg1);
         }
@@ -130,9 +141,11 @@ public class MathOperators {
             arg2 = NumberParser.parseNumber(arg2);
         }
         double divisor = arg2.getDouble();
+        // Check for division by zero
         if (divisor == 0.0) {
             throw new PerlCompilerException("Illegal division by zero");
         }
+        // Perform division
         return new RuntimeScalar(arg1.getDouble() / divisor);
     }
 
@@ -146,45 +159,101 @@ public class MathOperators {
     public static RuntimeScalar modulus(RuntimeScalar arg1, RuntimeScalar arg2) {
         int divisor = arg2.getInt();
         int result = arg1.getInt() % divisor;
+        // Adjust result for negative modulus
         if (result != 0.0 && ((divisor > 0.0 && result < 0.0) || (divisor < 0.0 && result > 0.0))) {
             result += divisor;
         }
         return new RuntimeScalar(result);
     }
 
+    /**
+     * Computes the natural logarithm of a RuntimeScalar.
+     *
+     * @param runtimeScalar The RuntimeScalar to compute the logarithm for.
+     * @return A new RuntimeScalar representing the natural logarithm.
+     */
     public static RuntimeScalar log(RuntimeScalar runtimeScalar) {
         return new RuntimeScalar(Math.log(runtimeScalar.getDouble()));
     }
 
+    /**
+     * Computes the square root of a RuntimeScalar.
+     *
+     * @param runtimeScalar The RuntimeScalar to compute the square root for.
+     * @return A new RuntimeScalar representing the square root.
+     */
     public static RuntimeScalar sqrt(RuntimeScalar runtimeScalar) {
         return new RuntimeScalar(Math.sqrt(runtimeScalar.getDouble()));
     }
 
+    /**
+     * Computes the cosine of a RuntimeScalar.
+     *
+     * @param runtimeScalar The RuntimeScalar to compute the cosine for.
+     * @return A new RuntimeScalar representing the cosine.
+     */
     public static RuntimeScalar cos(RuntimeScalar runtimeScalar) {
         return new RuntimeScalar(Math.cos(runtimeScalar.getDouble()));
     }
 
+    /**
+     * Computes the sine of a RuntimeScalar.
+     *
+     * @param runtimeScalar The RuntimeScalar to compute the sine for.
+     * @return A new RuntimeScalar representing the sine.
+     */
     public static RuntimeScalar sin(RuntimeScalar runtimeScalar) {
         return new RuntimeScalar(Math.sin(runtimeScalar.getDouble()));
     }
 
+    /**
+     * Computes the exponential of a RuntimeScalar.
+     *
+     * @param runtimeScalar The RuntimeScalar to compute the exponential for.
+     * @return A new RuntimeScalar representing the exponential.
+     */
     public static RuntimeScalar exp(RuntimeScalar runtimeScalar) {
         return new RuntimeScalar(Math.exp(runtimeScalar.getDouble()));
     }
 
+    /**
+     * Raises a RuntimeScalar to the power of another RuntimeScalar.
+     *
+     * @param runtimeScalar The base RuntimeScalar.
+     * @param arg           The exponent RuntimeScalar.
+     * @return A new RuntimeScalar representing the power.
+     */
     public static RuntimeScalar pow(RuntimeScalar runtimeScalar, RuntimeScalar arg) {
         return new RuntimeScalar(Math.pow(runtimeScalar.getDouble(), arg.getDouble()));
     }
 
+    /**
+     * Computes the angle theta (in radians) from the conversion of rectangular
+     * coordinates (x, y) to polar coordinates (r, theta). This method returns
+     * the angle whose tangent is the quotient of two specified numbers,
+     * effectively calculating the arc-tangent of y/x.
+     *
+     * @param runtimeScalar The y-coordinate as a RuntimeScalar.
+     * @param arg           The x-coordinate as a RuntimeScalar.
+     * @return A new RuntimeScalar representing the angle theta in radians.
+     */
     public static RuntimeScalar atan2(RuntimeScalar runtimeScalar, RuntimeScalar arg) {
         return new RuntimeScalar(Math.atan2(runtimeScalar.getDouble(), arg.getDouble()));
     }
 
+    /**
+     * Computes the absolute value of a RuntimeScalar.
+     *
+     * @param runtimeScalar The RuntimeScalar to compute the absolute value for.
+     * @return A new RuntimeScalar representing the absolute value.
+     */
     public static RuntimeScalar abs(RuntimeScalar runtimeScalar) {
         RuntimeScalar arg1 = runtimeScalar;
+        // Convert string type to number if necessary
         if (arg1.type == RuntimeScalarType.STRING) {
             arg1 = NumberParser.parseNumber(arg1);
         }
+        // Compute absolute value based on the type of RuntimeScalar
         if (arg1.type == RuntimeScalarType.DOUBLE) {
             return new RuntimeScalar(Math.abs(arg1.getDouble()));
         } else {
