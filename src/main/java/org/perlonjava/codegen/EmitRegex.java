9160e18dd04bccac1514d301f407907eab6bcf89
Flavio S. Glock <fglock@gmail.com>
2024-10-29 16:21:46 +0100

    docs

--- src/main/java/org/perlonjava/codegen/EmitRegex.java
+++ src/main/java/org/perlonjava/codegen/EmitRegex.java
@@ -4,7 +4,21 @@ import org.objectweb.asm.Opcodes;
 import org.perlonjava.astnode.*;
 import org.perlonjava.runtime.RuntimeContextType;
 
+/**
+ * The EmitRegex class is responsible for handling regex-related operations
+ * within the code generation process. It provides methods to handle binding
+ * and non-binding regex operations, as well as specific regex operations like
+ * transliteration and replacement.
+ */
 public class EmitRegex {
+
+    /**
+     * Handles the binding regex operation where a variable is bound to a regex operation.
+     * This method processes the binary operator node representing the binding operation.
+     *
+     * @param emitterVisitor The visitor used to emit bytecode.
+     * @param node The binary operator node representing the binding regex operation.
+     */
     static void handleBindRegex(EmitterVisitor emitterVisitor, BinaryOperatorNode node) {
         //
         //  BinaryOperatorNode: =~
@@ -15,29 +29,40 @@ public class EmitRegex {
         //        StringNode: 'abc'
         //        StringNode: 'i'
         //
-        EmitterVisitor scalarVisitor =
-                emitterVisitor.with(RuntimeContextType.SCALAR); // execute operands in scalar context
+
+        // Execute operands in scalar context
+        EmitterVisitor scalarVisitor = emitterVisitor.with(RuntimeContextType.SCALAR);
+
         if (node.right instanceof OperatorNode right) {
             if (right.operand instanceof ListNode) {
-                // regex operator:  $v =~ /regex/;
-                // bind the variable to the regex operation
+                // Regex operator: $v =~ /regex/;
+                // Bind the variable to the regex operation
                 ((ListNode) right.operand).elements.add(node.left);
                 right.accept(emitterVisitor);
                 return;
             }
         }
-        // not a regex operator:  $v =~ $qr;
+
+        // Not a regex operator: $v =~ $qr;
         node.right.accept(scalarVisitor);
         node.left.accept(scalarVisitor);
         emitterVisitor.pushCallContext();
         emitterVisitor.ctx.mv.visitMethodInsn(Opcodes.INVOKESTATIC,
                 "org/perlonjava/runtime/RuntimeRegex", "matchRegex",
                 "(Lorg/perlonjava/runtime/RuntimeScalar;Lorg/perlonjava/runtime/RuntimeScalar;I)Lorg/perlonjava/runtime/RuntimeDataProvider;", false);
+
+        // If the context type is VOID, pop the result off the stack
         if (emitterVisitor.ctx.contextType == RuntimeContextType.VOID) {
             emitterVisitor.ctx.mv.visitInsn(Opcodes.POP);
         }
     }
 
+    /**
+     * Handles the non-binding regex operation by wrapping the operation in a "not" and "scalar" context.
+     *
+     * @param emitterVisitor The visitor used to emit bytecode.
+     * @param node The binary operator node representing the non-binding regex operation.
+     */
     static void handleNotBindRegex(EmitterVisitor emitterVisitor, BinaryOperatorNode node) {
         emitterVisitor.visit(
                 new OperatorNode("not",
@@ -52,6 +77,12 @@ public class EmitRegex {
                 ));
     }
 
+    /**
+     * Handles various regex operations such as transliteration, replacement, and quoting.
+     *
+     * @param emitterVisitor The visitor used to emit bytecode.
+     * @param node The operator node representing the regex operation.
+     */
     static void handleRegex(EmitterVisitor emitterVisitor, OperatorNode node) {
         ListNode operand = (ListNode) node.operand;
         EmitterVisitor scalarVisitor = emitterVisitor.with(RuntimeContextType.SCALAR);
@@ -59,20 +90,22 @@ public class EmitRegex {
 
         switch (node.operator) {
             case "qx" -> {
-                // static RuntimeScalar systemCommand(RuntimeScalar command)
+                // Handle system command execution
                 operand.elements.getFirst().accept(scalarVisitor);
                 emitterVisitor.pushCallContext();
                 emitterVisitor.ctx.mv.visitMethodInsn(Opcodes.INVOKESTATIC,
                         "org/perlonjava/runtime/RuntimeIO",
                         "systemCommand",
                         "(Lorg/perlonjava/runtime/RuntimeScalar;I)Lorg/perlonjava/runtime/RuntimeDataProvider;", false);
+
+                // If the context type is VOID, pop the result off the stack
                 if (emitterVisitor.ctx.contextType == RuntimeContextType.VOID) {
                     emitterVisitor.ctx.mv.visitInsn(Opcodes.POP);
                 }
                 return;
             }
             case "tr", "y" -> {
-                // static RuntimeTransliterate compile(RuntimeScalar search, RuntimeScalar replace, RuntimeScalar modifiers)
+                // Handle transliteration operation
                 operand.elements.get(0).accept(scalarVisitor);
                 operand.elements.get(1).accept(scalarVisitor);
                 operand.elements.get(2).accept(scalarVisitor);
@@ -83,20 +116,22 @@ public class EmitRegex {
                         "org/perlonjava/operators/RuntimeTransliterate", "compile",
                         "(Lorg/perlonjava/runtime/RuntimeScalar;Lorg/perlonjava/runtime/RuntimeScalar;Lorg/perlonjava/runtime/RuntimeScalar;)Lorg/perlonjava/operators/RuntimeTransliterate;", false);
 
-                // RuntimeScalar transliterate(RuntimeScalar originalString)
+                // Handle transliteration of the original string
                 if (variable == null) {
-                    // use `$_`
+                    // Use `$_` if no variable is provided
                     variable = new OperatorNode("$", new IdentifierNode("_", node.tokenIndex), node.tokenIndex);
                 }
                 variable.accept(scalarVisitor);
                 emitterVisitor.ctx.mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "org/perlonjava/operators/RuntimeTransliterate", "transliterate", "(Lorg/perlonjava/runtime/RuntimeScalar;)Lorg/perlonjava/runtime/RuntimeScalar;", false);
+
+                // If the context type is VOID, pop the result off the stack
                 if (emitterVisitor.ctx.contextType == RuntimeContextType.VOID) {
                     emitterVisitor.ctx.mv.visitInsn(Opcodes.POP);
                 }
                 return;
             }
             case "replaceRegex" -> {
-                // RuntimeBaseEntity replaceRegex(RuntimeScalar quotedRegex, RuntimeScalar string, RuntimeScalar replacement, int ctx)
+                // Handle regex replacement operation
                 operand.elements.get(0).accept(scalarVisitor);
                 operand.elements.get(1).accept(scalarVisitor);
                 operand.elements.get(2).accept(scalarVisitor);
@@ -108,7 +143,7 @@ public class EmitRegex {
                         "(Lorg/perlonjava/runtime/RuntimeScalar;Lorg/perlonjava/runtime/RuntimeScalar;Lorg/perlonjava/runtime/RuntimeScalar;)Lorg/perlonjava/runtime/RuntimeScalar;", false);
             }
             default -> {
-                // RuntimeRegex.getQuotedRegex(RuntimeScalar patternString, RuntimeScalar modifiers)
+                // Handle quoted regex operation
                 operand.elements.get(0).accept(scalarVisitor);
                 operand.elements.get(1).accept(scalarVisitor);
                 if (operand.elements.size() > 2) {
@@ -121,12 +156,12 @@ public class EmitRegex {
         }
 
         if (node.operator.equals("quoteRegex")) {
-            // do not execute  `qr//`
+            // Do not execute `qr//`
             return;
         }
 
         if (variable == null) {
-            // use `$_`
+            // Use `$_` if no variable is provided
             variable = new OperatorNode("$", new IdentifierNode("_", node.tokenIndex), node.tokenIndex);
         }
         variable.accept(scalarVisitor);
@@ -136,6 +171,7 @@ public class EmitRegex {
                 "org/perlonjava/runtime/RuntimeRegex", "matchRegex",
                 "(Lorg/perlonjava/runtime/RuntimeScalar;Lorg/perlonjava/runtime/RuntimeScalar;I)Lorg/perlonjava/runtime/RuntimeDataProvider;", false);
 
+        // If the context type is VOID, pop the result off the stack
         if (emitterVisitor.ctx.contextType == RuntimeContextType.VOID) {
             emitterVisitor.ctx.mv.visitInsn(Opcodes.POP);
         }
