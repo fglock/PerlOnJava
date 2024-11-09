4f52ef1a5aa3e5cb035ec226b32de42f67abfd30
Flavio S. Glock <fglock@gmail.com>
2024-11-09 18:05:20 +0100

    special blocks wip: declare BEGIN closed variables

--- src/main/java/org/perlonjava/parser/SpecialBlock.java
+++ src/main/java/org/perlonjava/parser/SpecialBlock.java
@@ -9,11 +9,10 @@ import org.perlonjava.scriptengine.PerlLanguageProvider;
 
 import java.util.Map;
 
-import static org.perlonjava.runtime.GlobalContext.*;
 import static org.perlonjava.runtime.SpecialBlock.saveEndBlock;
 
 public class SpecialBlock {
-    
+
     static OperatorNode parseSpecialBlock(Parser parser) {
         String blockName = TokenUtils.consume(parser).text;
 
@@ -27,29 +26,34 @@ public class SpecialBlock {
         TokenUtils.consume(parser, LexerTokenType.OPERATOR, "}");
         int codeEnd = parser.tokenIndex;
 
-        ArgumentParser.CompilerOptions parsedArgs = parser.ctx.compilerOptions.clone();
-        parsedArgs.code = "sub " + TokenUtils.toText(parser.tokens, codeStart, codeEnd - 1);
-        parser.ctx.logDebug("Special block " + blockName + " <<<" + parsedArgs.code + ">>>");
-        parser.ctx.logDebug("Special block captures " + parser.ctx.symbolTable.getAllVisibleVariables());
+        String currentPackage = parser.ctx.symbolTable.getCurrentPackage();
 
-        //ScopedSymbolTable saveSymbolTable = outerSymbolTable;
-        //outerSymbolTable = parser.ctx.symbolTable;
+        ArgumentParser.CompilerOptions parsedArgs = parser.ctx.compilerOptions.clone();
+
+        StringBuilder codeSb = new StringBuilder();
+        codeSb.append("package ").append(currentPackage).append("; ");
 
         Map<Integer, SymbolTable.SymbolEntry> outerVars = parser.ctx.symbolTable.getAllVisibleVariables();
         for (SymbolTable.SymbolEntry entry : outerVars.values()) {
-            if (entry.decl().equals("our")) {
-                String sigil = entry.name().substring(0, 1);
-                String name = entry.perlPackage() + "::" + entry.name().substring(1);
-                // Creating the variable entries avoids the "requires explicit package name" error
-                // - this doesn't work if the "our" variable was declared in a different package
-                // - this doesn't work if the variable is declared with "my" or "state"
-                switch (sigil) {
-                    case "$" : getGlobalVariable(name);
-                    case "@" : getGlobalArray(name);
-                    case "%" : getGlobalHash(name);
+            // Creating `our $var;` entries avoids the "requires explicit package name" error
+            // XXX This doesn't do the right thing if the variable is declared with "my" or "state"
+            if (!entry.name().equals("@_") && !entry.decl().isEmpty()) {
+                boolean samePackage = !(entry.decl().equals("our")) || (currentPackage.equals(entry.perlPackage()));
+                if (!samePackage) {
+                    // "our" variable was declared in a different package
+                    codeSb.append("package ").append(entry.perlPackage()).append("; ");
+                }
+                codeSb.append(entry.decl()).append(" ").append(entry.name()).append("; ");
+                if (!samePackage) {
+                    codeSb.append("package ").append(currentPackage).append("; ");
                 }
             }
         }
+        codeSb.append("sub ").append(TokenUtils.toText(parser.tokens, codeStart, codeEnd - 1));
+
+        parsedArgs.code = codeSb.toString();
+        parser.ctx.logDebug("Special block " + blockName + " <<<" + parsedArgs.code + ">>>");
+        parser.ctx.logDebug("Special block captures " + parser.ctx.symbolTable.getAllVisibleVariables());
 
         try {
             RuntimeList result = PerlLanguageProvider.executePerlCode(parsedArgs, false);
@@ -68,7 +72,7 @@ public class SpecialBlock {
             throw new PerlCompilerException(parser.tokenIndex, message, parser.ctx.errorUtil);
         }
 
-        //outerSymbolTable = saveSymbolTable;
+        // XXX For BEGIN blocks, save the values of lexical variables and initialize them at run time
 
         return new OperatorNode("undef", null, parser.tokenIndex);
     }
