68fdf8cb07371935409afde6e79d8b0da81c3105
Flavio S. Glock <fglock@gmail.com>
2024-11-11 16:18:54 +0100

    refactoring

--- src/main/java/org/perlonjava/parser/SpecialBlockParser.java
+++ src/main/java/org/perlonjava/parser/SpecialBlockParser.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import static org.perlonjava.runtime.GlobalContext.*;
 import static org.perlonjava.runtime.SpecialBlock.*;
 
 /**
@@ -95,7 +94,7 @@ public class SpecialBlockParser {
                     // Emit: package BEGIN_PKG
                     nodes.add(
                             new OperatorNode("package",
-                                    new IdentifierNode(beginPackage(ast.id), tokenIndex), tokenIndex));
+                                    new IdentifierNode(PersistentVariable.beginPackage(ast.id), tokenIndex), tokenIndex));
                 }
                 // Emit: our $var
                 nodes.add(
@@ -165,65 +164,5 @@ public class SpecialBlockParser {
         }
 
         return result;
-    }
-
-    /**
-     * Constructs a package name for storing compile-time variables, with the given ID.
-     *
-     * @param id The ID of the BEGIN block.
-     * @return The package name for the BEGIN block.
-     */
-    static String beginPackage(int id) {
-        return "PerlOnJava::_BEGIN_" + id;
-    }
-
-    /**
-     * Constructs a compile-time variable name for a BEGIN block, with the given ID and name.
-     *
-     * @param id   The ID of the BEGIN block.
-     * @param name The name of the variable.
-     * @return The variable name for the BEGIN block.
-     */
-    static String beginVariable(int id, String name) {
-        return beginPackage(id) + "::" + name;
-    }
-
-    /**
-     * Retrieves a compile-time scalar variable.
-     *
-     * @param var The name of the variable.
-     * @param id  The ID of the BEGIN block.
-     * @return The retrieved RuntimeScalar.
-     */
-    public static RuntimeScalar retrieveBeginScalar(String var, int id) {
-        String beginVar = beginVariable(id, var.substring(1));
-        RuntimeScalar temp = removeGlobalVariable(beginVar);
-        return temp == null ? new RuntimeScalar() : temp;
-    }
-
-    /**
-     * Retrieves a compile-time array variable.
-     *
-     * @param var The name of the variable.
-     * @param id  The ID of the BEGIN block.
-     * @return The retrieved RuntimeArray.
-     */
-    public static RuntimeArray retrieveBeginArray(String var, int id) {
-        String beginVar = beginVariable(id, var.substring(1));
-        RuntimeArray temp = removeGlobalArray(beginVar);
-        return temp == null ? new RuntimeArray() : temp;
-    }
-
-    /**
-     * Retrieves a compile-time hash variable.
-     *
-     * @param var The name of the variable.
-     * @param id  The ID of the BEGIN block.
-     * @return The retrieved RuntimeHash.
-     */
-    public static RuntimeHash retrieveBeginHash(String var, int id) {
-        String beginVar = beginVariable(id, var.substring(1));
-        RuntimeHash temp = removeGlobalHash(beginVar);
-        return temp == null ? new RuntimeHash() : temp;
     }
 }
