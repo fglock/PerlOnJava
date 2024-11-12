3cfc331f080f7c36f550f01c99b70b242980343e
Flavio S. Glock <fglock@gmail.com>
2024-11-12 13:49:07 +0100

    docs

--- src/main/java/org/perlonjava/runtime/SpecialBlock.java
+++ src/main/java/org/perlonjava/runtime/SpecialBlock.java
@@ -1,23 +1,54 @@
 package org.perlonjava.runtime;
 
+/**
+ * The SpecialBlock class manages different types of code blocks (end, init, check, and unitcheck)
+ * that can be saved and executed in a specific order. This class provides methods to save and run
+ * these blocks, ensuring they are executed based on their defined order and conditions.
+ *
+ * The order of execution is influenced by how blocks are added:
+ * - Blocks added with `push` are executed in Last-In-First-Out (LIFO) order.
+ * - Blocks added with `unshift` are executed in First-In-First-Out (FIFO) order.
+ */
 public class SpecialBlock {
 
+    // Arrays to store different types of blocks
     public static RuntimeArray endBlocks = new RuntimeArray();
     public static RuntimeArray initBlocks = new RuntimeArray();
     public static RuntimeArray checkBlocks = new RuntimeArray();
 
+    /**
+     * Saves a code reference to the endBlocks array.
+     * Blocks are added using `push`, meaning they will be executed in LIFO order.
+     *
+     * @param codeRef the code reference to be saved
+     */
     public static void saveEndBlock(RuntimeScalar codeRef) {
         endBlocks.push(codeRef);
     }
 
+    /**
+     * Saves a code reference to the initBlocks array.
+     * Blocks are added using `unshift`, meaning they will be executed in FIFO order.
+     *
+     * @param codeRef the code reference to be saved
+     */
     public static void saveInitBlock(RuntimeScalar codeRef) {
         initBlocks.unshift(codeRef);
     }
 
+    /**
+     * Saves a code reference to the checkBlocks array.
+     * Blocks are added using `push`, meaning they will be executed in LIFO order.
+     *
+     * @param codeRef the code reference to be saved
+     */
     public static void saveCheckBlock(RuntimeScalar codeRef) {
-        checkBlocks.push(codeRef);  // runs in LIFO order
+        checkBlocks.push(codeRef);
     }
 
+    /**
+     * Executes all code blocks stored in the endBlocks array in LIFO order.
+     */
     public static void runEndBlocks() {
         while (endBlocks.size() > 0) {
             RuntimeScalar block = endBlocks.pop();
@@ -27,6 +58,9 @@ public class SpecialBlock {
         }
     }
 
+    /**
+     * Executes all code blocks stored in the initBlocks array in FIFO order.
+     */
     public static void runInitBlocks() {
         while (initBlocks.size() > 0) {
             RuntimeScalar block = initBlocks.pop();
@@ -36,6 +70,9 @@ public class SpecialBlock {
         }
     }
 
+    /**
+     * Executes all code blocks stored in the checkBlocks array in LIFO order.
+     */
     public static void runCheckBlocks() {
         while (checkBlocks.size() > 0) {
             RuntimeScalar block = checkBlocks.pop();
@@ -45,6 +82,11 @@ public class SpecialBlock {
         }
     }
 
+    /**
+     * Executes all code blocks stored in the provided unitcheckBlocks array in LIFO order.
+     *
+     * @param unitcheckBlocks the array of code blocks to be executed
+     */
     public static void runUnitcheckBlocks(RuntimeArray unitcheckBlocks) {
         while (unitcheckBlocks.size() > 0) {
             RuntimeScalar block = unitcheckBlocks.pop();
