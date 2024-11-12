c88eebe11bd83d20c03df7d9dc68cf69af54511f
Flavio S. Glock <fglock@gmail.com>
2024-11-12 14:29:10 +0100

    docs

--- src/main/java/org/perlonjava/runtime/ExceptionFormatter.java
+++ src/main/java/org/perlonjava/runtime/ExceptionFormatter.java
@@ -3,14 +3,30 @@ package org.perlonjava.runtime;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * The ExceptionFormatter class provides utility methods for formatting exceptions.
+ * It extracts and formats the stack trace of the innermost cause of a given Throwable.
+ */
 public class ExceptionFormatter {
 
+    /**
+     * Formats the innermost cause of the given Throwable into a structured stack trace.
+     *
+     * @param t The Throwable to format.
+     * @return A list of lists, where each inner list contains the package name, source file name,
+     * and line number of a stack trace element.
+     */
     public static ArrayList<ArrayList<String>> formatException(Throwable t) {
         Throwable innermostCause = findInnermostCause(t);
         return formatThrowable(innermostCause);
     }
 
-    // Find the innermost cause in the exception chain
+    /**
+     * Finds the innermost cause in the exception chain of the given Throwable.
+     *
+     * @param t The Throwable to inspect.
+     * @return The innermost Throwable cause.
+     */
     public static Throwable findInnermostCause(Throwable t) {
         Throwable cause = t;
         while (cause.getCause() != null) {
@@ -19,28 +35,35 @@ public class ExceptionFormatter {
         return cause;
     }
 
+    /**
+     * Formats the stack trace of the given Throwable into a structured list.
+     * This method emulates the Perl method "Carp::longmess".
+     * It only includes stack trace elements that contain the method name "apply" and do not
+     * originate from Java source files.
+     *
+     * @param t The Throwable whose stack trace is to be formatted.
+     * @return A list of lists, where each inner list contains the package name, source file name,
+     * and line number of a relevant stack trace element.
+     */
     private static ArrayList<ArrayList<String>> formatThrowable(Throwable t) {
-
         ArrayList<ArrayList<String>> stackTrace = new ArrayList<>();
-
-        // Append the main message of the exception
-        // sb.append(t.getClass().getName()).append(": ").append(t.getMessage()).append("\n");
-
-        // Note: filename is formatted in codegen.DebugInfo like:
-        // sourceFileName + " @ " + packageName
 
         // Filter and append the stack trace
         Arrays.stream(t.getStackTrace())
                 .filter(element ->
-                        element.getMethodName().contains("apply")
-                                && !element.getFileName().contains(".java")
+                        element.getMethodName().contains("apply") // Only include methods with "apply" in their name
+                                && !element.getFileName().contains(".java") // Exclude Java source files
                 )
                 .forEach(element -> {
                     String fileName = element.getFileName();
+                    // Split the file name into source file name and package name.
+                    // The filename is formatted in codegen.DebugInfo like:
+                    // sourceFileName + " @ " + packageName
                     String[] fileNameParts = fileName.split(" @ ", 2); // Split into at most 2 parts
                     String sourceFileName = fileNameParts[0];
                     String packageName = fileNameParts.length > 1 ? fileNameParts[1] : "";
 
+                    // Add the formatted stack trace element to the list
                     stackTrace.add(
                             new ArrayList<>(
                                     Arrays.asList(
@@ -54,4 +77,3 @@ public class ExceptionFormatter {
         return stackTrace;
     }
 }
-
