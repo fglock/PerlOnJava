12707959d5e359d48f5f2d5586d5cabba1cebb05
Flavio S. Glock <fglock@gmail.com>
2024-11-13 20:43:42 +0100

    glob assignment wip

--- src/main/java/org/perlonjava/codegen/EmitVariable.java
+++ src/main/java/org/perlonjava/codegen/EmitVariable.java
@@ -191,6 +191,11 @@ public class EmitVariable {
                 boolean isGlob = false;
                 String leftDescriptor = "org/perlonjava/runtime/RuntimeScalar";
                 if (nodeLeft != null && nodeLeft.operator.equals("*")) {
+                    // glob:  *var
+
+                    // XXX TODO Identify a typeglob, formed by symbol table entry + key:
+                    //   $constant::{_CAN_PCS} = \$const;
+                    
                     leftDescriptor = "org/perlonjava/runtime/RuntimeGlob";
                     isGlob = true;
                 }
