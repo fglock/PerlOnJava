665f2b28c7f01c1f729564326e44b3c0add2d806
Flavio S. Glock <fglock@gmail.com>
2024-11-18 13:21:40 +0100

    "bitwise" feature wip

--- src/main/java/org/perlonjava/operators/BitwiseOperators.java
+++ src/main/java/org/perlonjava/operators/BitwiseOperators.java
@@ -12,99 +12,6 @@ import org.perlonjava.runtime.RuntimeScalarType;
 public class BitwiseOperators {
 
     /**
-     * Helper method for performing a bitwise AND operation on two strings.
-     *
-     * @param s1 The first string operand.
-     * @param s2 The second string operand.
-     * @return The result of the bitwise AND operation as a string.
-     * @throws PerlCompilerException if the strings contain characters with code points over 0xFF.
-     */
-    private static String bitwiseAndStrings(String s1, String s2) {
-        int len = Math.min(s1.length(), s2.length());
-        StringBuilder result = new StringBuilder(len);
-
-        for (int i = 0; i < len; i++) {
-            char c1 = s1.charAt(i);
-            char c2 = s2.charAt(i);
-            if (c1 > 0xFF || c2 > 0xFF) {
-                throw new PerlCompilerException("Use of strings with code points over 0xFF as arguments to bitwise and (&) operator is not allowed");
-            }
-            result.append((char) (c1 & c2));
-        }
-
-        return result.toString();
-    }
-
-    /**
-     * Helper method for performing a bitwise OR operation on two strings.
-     *
-     * @param s1 The first string operand.
-     * @param s2 The second string operand.
-     * @return The result of the bitwise OR operation as a string.
-     * @throws PerlCompilerException if the strings contain characters with code points over 0xFF.
-     */
-    private static String bitwiseOrStrings(String s1, String s2) {
-        int len = Math.max(s1.length(), s2.length());
-        StringBuilder result = new StringBuilder(len);
-
-        for (int i = 0; i < len; i++) {
-            char c1 = i < s1.length() ? s1.charAt(i) : 0;
-            char c2 = i < s2.length() ? s2.charAt(i) : 0;
-            if (c1 > 0xFF || c2 > 0xFF) {
-                throw new PerlCompilerException("Use of strings with code points over 0xFF as arguments to bitwise or (|) operator is not allowed");
-            }
-            result.append((char) (c1 | c2));
-        }
-
-        return result.toString();
-    }
-
-    /**
-     * Helper method for performing a bitwise XOR operation on two strings.
-     *
-     * @param s1 The first string operand.
-     * @param s2 The second string operand.
-     * @return The result of the bitwise XOR operation as a string.
-     * @throws PerlCompilerException if the strings contain characters with code points over 0xFF.
-     */
-    private static String bitwiseXorStrings(String s1, String s2) {
-        int len = Math.max(s1.length(), s2.length());
-        StringBuilder result = new StringBuilder(len);
-
-        for (int i = 0; i < len; i++) {
-            char c1 = i < s1.length() ? s1.charAt(i) : 0;
-            char c2 = i < s2.length() ? s2.charAt(i) : 0;
-            if (c1 > 0xFF || c2 > 0xFF) {
-                throw new PerlCompilerException("Use of strings with code points over 0xFF as arguments to bitwise xor (^) operator is not allowed");
-            }
-            result.append((char) (c1 ^ c2));
-        }
-
-        return result.toString();
-    }
-
-    /**
-     * Helper method for performing a bitwise NOT operation on a string.
-     *
-     * @param s The string operand.
-     * @return The result of the bitwise NOT operation as a string.
-     * @throws PerlCompilerException if the string contains characters with code points over 0xFF.
-     */
-    private static String bitwiseNotString(String s) {
-        StringBuilder result = new StringBuilder(s.length());
-
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c > 0xFF) {
-                throw new PerlCompilerException("Use of strings with code points over 0xFF as arguments to bitwise not (~) operator is not allowed");
-            }
-            result.append((char) ((~c) & 0xFF));
-        }
-
-        return result.toString();
-    }
-
-    /**
      * Performs a bitwise AND operation on two RuntimeScalar objects.
      * If both arguments are strings, it performs the operation character by character.
      *
@@ -114,8 +21,19 @@ public class BitwiseOperators {
      */
     public static RuntimeScalar bitwiseAnd(RuntimeScalar runtimeScalar, RuntimeScalar arg2) {
         if (runtimeScalar.type == RuntimeScalarType.STRING && arg2.type == RuntimeScalarType.STRING) {
-            return new RuntimeScalar(bitwiseAndStrings(runtimeScalar.toString(), arg2.toString()));
+            return bitwiseAndDot(runtimeScalar, arg2);
         }
+        return bitwiseAndBinary(runtimeScalar, arg2);
+    }
+
+    /**
+     * Performs a bitwise AND operation on two numeric RuntimeScalar objects.
+     *
+     * @param runtimeScalar The first operand.
+     * @param arg2          The second operand.
+     * @return A new RuntimeScalar with the result of the bitwise AND operation.
+     */
+    public static RuntimeScalar bitwiseAndBinary(RuntimeScalar runtimeScalar, RuntimeScalar arg2) {
         return new RuntimeScalar(runtimeScalar.getLong() & arg2.getLong());
     }
 
@@ -129,8 +47,19 @@ public class BitwiseOperators {
      */
     public static RuntimeScalar bitwiseOr(RuntimeScalar runtimeScalar, RuntimeScalar arg2) {
         if (runtimeScalar.type == RuntimeScalarType.STRING && arg2.type == RuntimeScalarType.STRING) {
-            return new RuntimeScalar(bitwiseOrStrings(runtimeScalar.toString(), arg2.toString()));
+            return bitwiseOrDot(runtimeScalar, arg2);
         }
+        return bitwiseOrBinary(runtimeScalar, arg2);
+    }
+
+    /**
+     * Performs a bitwise OR operation on two numeric RuntimeScalar objects.
+     *
+     * @param runtimeScalar The first operand.
+     * @param arg2          The second operand.
+     * @return A new RuntimeScalar with the result of the bitwise OR operation.
+     */
+    public static RuntimeScalar bitwiseOrBinary(RuntimeScalar runtimeScalar, RuntimeScalar arg2) {
         return new RuntimeScalar(runtimeScalar.getLong() | arg2.getLong());
     }
 
@@ -144,8 +73,19 @@ public class BitwiseOperators {
      */
     public static RuntimeScalar bitwiseXor(RuntimeScalar runtimeScalar, RuntimeScalar arg2) {
         if (runtimeScalar.type == RuntimeScalarType.STRING && arg2.type == RuntimeScalarType.STRING) {
-            return new RuntimeScalar(bitwiseXorStrings(runtimeScalar.toString(), arg2.toString()));
+            return bitwiseXorDot(runtimeScalar, arg2);
         }
+        return bitwiseXorBinary(runtimeScalar, arg2);
+    }
+
+    /**
+     * Performs a bitwise XOR operation on two numeric RuntimeScalar objects.
+     *
+     * @param runtimeScalar The first operand.
+     * @param arg2          The second operand.
+     * @return A new RuntimeScalar with the result of the bitwise XOR operation.
+     */
+    public static RuntimeScalar bitwiseXorBinary(RuntimeScalar runtimeScalar, RuntimeScalar arg2) {
         return new RuntimeScalar(runtimeScalar.getLong() ^ arg2.getLong());
     }
 
@@ -158,8 +98,18 @@ public class BitwiseOperators {
      */
     public static RuntimeScalar bitwiseNot(RuntimeScalar runtimeScalar) {
         if (runtimeScalar.type == RuntimeScalarType.STRING) {
-            return new RuntimeScalar(bitwiseNotString(runtimeScalar.toString()));
+            return bitwiseNotDot(runtimeScalar);
         }
+        return bitwiseNotBinary(runtimeScalar);
+    }
+
+    /**
+     * Performs a bitwise NOT operation on a numeric RuntimeScalar object.
+     *
+     * @param runtimeScalar The operand.
+     * @return A new RuntimeScalar with the result of the bitwise NOT operation.
+     */
+    public static RuntimeScalar bitwiseNotBinary(RuntimeScalar runtimeScalar) {
         return new RuntimeScalar(~runtimeScalar.getLong() & 0xFFFFFFFFL);
     }
 
@@ -172,7 +122,21 @@ public class BitwiseOperators {
      * @return A new RuntimeScalar with the result of the bitwise AND operation on the string.
      */
     public static RuntimeScalar bitwiseAndDot(RuntimeScalar runtimeScalar, RuntimeScalar arg2) {
-        return new RuntimeScalar(bitwiseAndStrings(runtimeScalar.toString(), arg2.toString()));
+        String s1 = runtimeScalar.toString();
+        String s2 = arg2.toString();
+        int len = Math.min(s1.length(), s2.length());
+        StringBuilder result = new StringBuilder(len);
+
+        for (int i = 0; i < len; i++) {
+            char c1 = s1.charAt(i);
+            char c2 = s2.charAt(i);
+            if (c1 > 0xFF || c2 > 0xFF) {
+                throw new PerlCompilerException("Use of strings with code points over 0xFF as arguments to bitwise and (&) operator is not allowed");
+            }
+            result.append((char) (c1 & c2));
+        }
+
+        return new RuntimeScalar(result.toString());
     }
 
     /**
@@ -184,7 +148,21 @@ public class BitwiseOperators {
      * @return A new RuntimeScalar with the result of the bitwise OR operation on the string.
      */
     public static RuntimeScalar bitwiseOrDot(RuntimeScalar runtimeScalar, RuntimeScalar arg2) {
-        return new RuntimeScalar(bitwiseOrStrings(runtimeScalar.toString(), arg2.toString()));
+        String s1 = runtimeScalar.toString();
+        String s2 = arg2.toString();
+        int len = Math.max(s1.length(), s2.length());
+        StringBuilder result = new StringBuilder(len);
+
+        for (int i = 0; i < len; i++) {
+            char c1 = i < s1.length() ? s1.charAt(i) : 0;
+            char c2 = i < s2.length() ? s2.charAt(i) : 0;
+            if (c1 > 0xFF || c2 > 0xFF) {
+                throw new PerlCompilerException("Use of strings with code points over 0xFF as arguments to bitwise or (|) operator is not allowed");
+            }
+            result.append((char) (c1 | c2));
+        }
+
+        return new RuntimeScalar(result.toString());
     }
 
     /**
@@ -196,7 +174,21 @@ public class BitwiseOperators {
      * @return A new RuntimeScalar with the result of the bitwise XOR operation on the string.
      */
     public static RuntimeScalar bitwiseXorDot(RuntimeScalar runtimeScalar, RuntimeScalar arg2) {
-        return new RuntimeScalar(bitwiseXorStrings(runtimeScalar.toString(), arg2.toString()));
+        String s1 = runtimeScalar.toString();
+        String s2 = arg2.toString();
+        int len = Math.max(s1.length(), s2.length());
+        StringBuilder result = new StringBuilder(len);
+
+        for (int i = 0; i < len; i++) {
+            char c1 = i < s1.length() ? s1.charAt(i) : 0;
+            char c2 = i < s2.length() ? s2.charAt(i) : 0;
+            if (c1 > 0xFF || c2 > 0xFF) {
+                throw new PerlCompilerException("Use of strings with code points over 0xFF as arguments to bitwise xor (^) operator is not allowed");
+            }
+            result.append((char) (c1 ^ c2));
+        }
+
+        return new RuntimeScalar(result.toString());
     }
 
     /**
@@ -207,7 +199,18 @@ public class BitwiseOperators {
      * @return A new RuntimeScalar with the result of the bitwise NOT operation on the string.
      */
     public static RuntimeScalar bitwiseNotDot(RuntimeScalar runtimeScalar) {
-        return new RuntimeScalar(bitwiseNotString(runtimeScalar.toString()));
+        String s = runtimeScalar.toString();
+        StringBuilder result = new StringBuilder(s.length());
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c > 0xFF) {
+                throw new PerlCompilerException("Use of strings with code points over 0xFF as arguments to bitwise not (~) operator is not allowed");
+            }
+            result.append((char) ((~c) & 0xFF));
+        }
+
+        return new RuntimeScalar(result.toString());
     }
 
     /**
