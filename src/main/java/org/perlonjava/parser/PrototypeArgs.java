4ca32ff4bdde1499cbd231f0c565c35343c9da47
fglock <fglock@gmail.com>
2024-11-29 20:16:56 +0100

    subroutine prototype wip

--- src/main/java/org/perlonjava/parser/PrototypeArgs.java
+++ src/main/java/org/perlonjava/parser/PrototypeArgs.java
@@ -3,6 +3,7 @@ package org.perlonjava.parser;
 import org.perlonjava.astnode.ListNode;
 import org.perlonjava.astnode.Node;
 import org.perlonjava.astnode.OperatorNode;
+import org.perlonjava.astnode.SubroutineNode;
 import org.perlonjava.lexer.LexerToken;
 import org.perlonjava.lexer.LexerTokenType;
 import org.perlonjava.runtime.PerlCompilerException;
@@ -70,6 +71,42 @@ public class PrototypeArgs {
                         args.elements.add(element);
                     }
                     needComma = true;
+                    prototype = prototype.substring(1);
+                } else if (prototype.startsWith("&")) {
+                    // System.out.println("prototype consume &");
+                    if (needComma) {
+                        LexerToken token = TokenUtils.peek(parser);
+                        if (!token.text.equals(",") && !token.text.equals("=>")) {
+                            if (isOptional) {
+                                break;
+                            }
+                            throw new PerlCompilerException("syntax error, expected comma");
+                        }
+                        TokenUtils.consume(parser);
+                    }
+                    if (TokenUtils.peek(parser).text.equals("{")) {
+                        //  { ... }
+                        TokenUtils.consume(parser);
+                        Node block = parser.parseBlock();
+                        block = new SubroutineNode(null, null, null, block, false, parser.tokenIndex);
+                        TokenUtils.consume(parser, LexerTokenType.OPERATOR, "}");
+                        args.elements.add(block);
+                        needComma = false;
+                    } else if (TokenUtils.peek(parser).text.equals("\\")) {
+                        //  \&sub
+                        ListNode argList = ListParser.parseZeroOrOneList(parser, 0);
+                        if (argList.elements.size() == 0) {
+                            if (isOptional) {
+                                break;
+                            }
+                            throw new PerlCompilerException("syntax error, expected coderef");
+                        }
+                        Node element = argList.elements.getFirst();
+                        args.elements.add(element);
+                        needComma = true;
+                    } else {
+                        throw new PerlCompilerException("syntax error, expected block");
+                    }
                     prototype = prototype.substring(1);
                 } else {
                     throw new PerlCompilerException("syntax error, unexpected prototype " + prototype);
