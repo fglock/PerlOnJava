d191560cadcd7869a3d1756620cbde4681a9d957
Flavio S. Glock <fglock@gmail.com>
2024-12-05 12:02:31 +0100

    io refactoring - cleanup

--- src/main/java/org/perlonjava/io/IOHandle.java
+++ src/main/java/org/perlonjava/io/IOHandle.java
@@ -1,5 +1,6 @@
 package org.perlonjava.io;
 
+import org.perlonjava.runtime.RuntimeIO;
 import org.perlonjava.runtime.RuntimeScalar;
 
 public interface IOHandle {
@@ -11,47 +12,47 @@ public interface IOHandle {
     RuntimeScalar flush();
 
     default RuntimeScalar getc() {
-        throw new UnsupportedOperationException("getc operation is not supported.");
+        return RuntimeIO.handleIOError("getc operation is not supported.");
     }
 
     default RuntimeScalar fileno() {
-        throw new UnsupportedOperationException("fileno operation is not supported.");
+        return RuntimeIO.handleIOError("fileno operation is not supported.");
     }
 
     default RuntimeScalar eof() {
-        throw new UnsupportedOperationException("eof operation is not supported.");
+        return RuntimeIO.handleIOError("eof operation is not supported.");
     }
 
     default RuntimeScalar read(byte[] buffer) {
-        throw new UnsupportedOperationException("read operation is not supported.");
+        return RuntimeIO.handleIOError("read operation is not supported.");
     }
 
     default RuntimeScalar tell() {
-        throw new UnsupportedOperationException("tell operation is not supported.");
+        return RuntimeIO.handleIOError("tell operation is not supported.");
     }
 
     // Socket-specific methods
     default RuntimeScalar bind(String address, int port) {
-        throw new UnsupportedOperationException("Bind operation is not supported.");
+        return RuntimeIO.handleIOError("Bind operation is not supported.");
     }
 
     default RuntimeScalar connect(String address, int port) {
-        throw new UnsupportedOperationException("Connect operation is not supported.");
+        return RuntimeIO.handleIOError("Connect operation is not supported.");
     }
 
     default RuntimeScalar listen(int backlog) {
-        throw new UnsupportedOperationException("Listen operation is not supported.");
+        return RuntimeIO.handleIOError("Listen operation is not supported.");
     }
 
     default RuntimeScalar accept() {
-        throw new UnsupportedOperationException("Accept operation is not supported.");
+        return RuntimeIO.handleIOError("Accept operation is not supported.");
     }
 
     default RuntimeScalar seek(long pos) {
-        throw new UnsupportedOperationException("Seek operation is not supported.");
+        return RuntimeIO.handleIOError("Seek operation is not supported.");
     }
 
     default RuntimeScalar truncate(long length) {
-        throw new UnsupportedOperationException("Truncate operation is not supported.");
+        return RuntimeIO.handleIOError("Truncate operation is not supported.");
     }
 }
