d8a97169d56b1639871a8d563c55ea3935d5910b
Flavio S. Glock <fglock@gmail.com>
2024-12-05 15:59:38 +0100

    refactoring

--- src/main/java/org/perlonjava/operators/Random.java
+++ src/main/java/org/perlonjava/operators/Random.java
@@ -3,10 +3,25 @@ package org.perlonjava.operators;
 import org.perlonjava.runtime.RuntimeScalar;
 import org.perlonjava.runtime.RuntimeScalarType;
 
+/**
+ * The Random class provides methods to generate random numbers and manage random seeds.
+ */
 public class Random {
+    /**
+     * The current seed used for random number generation.
+     */
     public static long currentSeed = System.currentTimeMillis();
+
     private static final java.util.Random random = new java.util.Random(currentSeed);
 
+    /**
+     * Sets a new seed for the random number generator. If a seed is provided via the
+     * {@link RuntimeScalar} parameter, it is used as the new seed. Otherwise, a semi-random
+     * seed is generated.
+     *
+     * @param runtimeScalar A {@link RuntimeScalar} object that may contain a seed value.
+     * @return A {@link RuntimeScalar} containing the old seed value.
+     */
     public static RuntimeScalar srand(RuntimeScalar runtimeScalar) {
         long oldSeed = currentSeed;
         if (runtimeScalar.type != RuntimeScalarType.UNDEF) {
@@ -19,6 +34,12 @@ public class Random {
         return new RuntimeScalar(oldSeed);
     }
 
+    /**
+     * Generates a random number scaled by the value provided in the {@link RuntimeScalar} parameter.
+     *
+     * @param runtimeScalar A {@link RuntimeScalar} object that provides the scaling factor.
+     * @return A {@link RuntimeScalar} containing the scaled random number.
+     */
     public static RuntimeScalar rand(RuntimeScalar runtimeScalar) {
         return new RuntimeScalar(random.nextDouble() * runtimeScalar.getDouble());
     }
