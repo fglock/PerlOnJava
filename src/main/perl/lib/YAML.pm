5c5fe35a7b7e849b28e73b6947822d9247717b68
Flavio S. Glock <fglock@gmail.com>
2025-01-23 11:54:20 +0100

    `YAML` module - use Perl subroutines

--- src/main/perl/lib/YAML.pm
+++ src/main/perl/lib/YAML.pm
@@ -2,36 +2,11 @@ package YAML;
 
 use strict;
 use warnings;
-use YAML::PP;
+use YAML::PP qw(Load Dump LoadFile DumpFile);
 use Exporter 'import';
 
 our @EXPORT = qw(Load Dump);
 our @EXPORT_OK = qw(LoadFile DumpFile);
 our $VERSION = '0.01';
 
-my $ypp = YAML::PP->new;
-
-sub Load {
-    my ($input) = @_;
-    my @docs = $ypp->load_string($input);
-    return wantarray ? @docs : $docs[0];
-}
-
-sub Dump {
-    my @docs = @_;
-    return $ypp->dump_string(@docs);
-}
-
-sub LoadFile {
-    my ($filename) = @_;
-    my @docs = $ypp->load_file($filename);
-    return wantarray ? @docs : $docs[0];
-}
-
-sub DumpFile {
-    my ($filename, @docs) = @_;
-    return $ypp->dump_file($filename, @docs);
-}
-
 1;
-
