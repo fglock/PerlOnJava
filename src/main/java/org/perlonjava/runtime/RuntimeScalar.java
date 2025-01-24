0fd92f02da848548ce847fd79f133c7e83e1caf3
Flavio S. Glock <fglock@gmail.com>
2025-01-24 10:57:11 +0100

    fix defined(&code)

--- src/main/java/org/perlonjava/runtime/RuntimeScalar.java
+++ src/main/java/org/perlonjava/runtime/RuntimeScalar.java
@@ -520,8 +520,8 @@ public class RuntimeScalar extends RuntimeBaseEntity implements RuntimeScalarRef
     }
 
     public RuntimeScalar defined() {
-        if (type == RuntimeScalarType.CODE && !((RuntimeCode) value).defined()) {
-            return scalarFalse;
+        if (type == RuntimeScalarType.CODE) {
+            return ((RuntimeCode) value).defined() ? scalarTrue : scalarFalse;
         }
         if (type == RuntimeScalarType.BOOLEAN) {
             return (boolean) value ? scalarTrue : scalarFalse;
@@ -530,10 +530,8 @@ public class RuntimeScalar extends RuntimeBaseEntity implements RuntimeScalarRef
     }
 
     public boolean getDefinedBoolean() {
-        if (type == RuntimeScalarType.GLOB && value == null) {
-            // getGlobalCodeRef returns an "empty glob" if the codeRef is not set
-            // XXX TODO implement a better response in getGlobalCodeRef
-            return false;
+        if (type == RuntimeScalarType.CODE) {
+            return ((RuntimeCode) value).defined();
         }
         if (type == RuntimeScalarType.BOOLEAN) {
             return (boolean) value;
