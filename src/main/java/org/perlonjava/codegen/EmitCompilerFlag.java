6d79fb28f8e85318df38649b755769b3ed9c9c35
Flavio S. Glock <fglock@gmail.com>
2025-02-13 14:23:44 +0100

    fix context in CompilerFlagNode

---
+++ src/main/java/org/perlonjava/codegen/EmitCompilerFlag.java
@@ -0,0 +1,24 @@
+package org.perlonjava.codegen;
+
+import org.perlonjava.astnode.CompilerFlagNode;
+import org.perlonjava.symbols.ScopedSymbolTable;
+
+public class EmitCompilerFlag {
+    public static void emitCompilerFlag(EmitterContext ctx, CompilerFlagNode node) {
+        ScopedSymbolTable currentScope = ctx.symbolTable;
+
+        // Set the warning flags
+        currentScope.warningFlagsStack.pop();
+        currentScope.warningFlagsStack.push(node.getWarningFlags());
+
+        // Set the feature flags
+        currentScope.featureFlagsStack.pop();
+        currentScope.featureFlagsStack.push(node.getFeatureFlags());
+
+        // Set the strict options
+        currentScope.strictOptionsStack.pop();
+        currentScope.strictOptionsStack.push(node.getStrictOptions());
+
+        EmitterContext.fixupContext(ctx);
+    }
+}
