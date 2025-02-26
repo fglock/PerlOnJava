92f69e5f93367f9ef761c074a1a91f089e0d5974
Flavio S. Glock <fglock@gmail.com>
2025-02-26 15:56:46 +0100

    fix stack trace in eval - https://github.com/fglock/PerlOnJava/issues/10

--- src/main/java/org/perlonjava/runtime/ExceptionFormatter.java
+++ src/main/java/org/perlonjava/runtime/ExceptionFormatter.java
@@ -4,6 +4,7 @@ import org.perlonjava.codegen.ByteCodeSourceMapper;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * The ExceptionFormatter class provides utility methods for formatting exceptions.
@@ -38,44 +39,52 @@ public class ExceptionFormatter {
     }
 
     /**
-     * Formats the stack trace of the given Throwable into a structured list.
-     * This method emulates the Perl method "Carp::longmess".
-     * It only includes stack trace elements that contain the method name "apply" and do not
-     * originate from Java source files.
+     * Formats the stack trace of a Throwable, replacing specific entries with artificial caller stack entries.
      *
      * @param t The Throwable whose stack trace is to be formatted.
-     * @return A list of lists, where each inner list contains the package name, source file name,
-     * and line number of a relevant stack trace element.
+     * @return A list of lists, where each inner list represents a stack trace element with package name, source file, and line number.
      */
     private static ArrayList<ArrayList<String>> formatThrowable(Throwable t) {
         ArrayList<ArrayList<String>> stackTrace = new ArrayList<>();
-
-        // System.out.println("innermostCause: "); innermostCause.printStackTrace();
-
-        // Filter and append the stack trace
-
-        // Filter compiled Perl methods like:
-        //     org.perlonjava.anon1.apply(misc/snippets/CallerTest.pm @ CallerTest:36)
-        // Filter `org.perlonjava.perlmodule` Perl-like Java methods like:
-        //     org.perlonjava.perlmodule.Exporter.exportOkTags(Exporter.java:159)
+        AtomicInteger callerStackIndex = new AtomicInteger(); // Initialize the index for CallerStack
 
         Arrays.stream(t.getStackTrace())
-                .filter(element ->
-                        element.getClassName().contains("org.perlonjava.anon")
-                                || element.getClassName().contains("org.perlonjava.perlmodule")
-                )
                 .forEach(element -> {
-                    ByteCodeSourceMapper.SourceLocation loc = ByteCodeSourceMapper.parseStackTraceElement(element);
-                    stackTrace.add(
-                            new ArrayList<>(
-                                    Arrays.asList(
-                                            loc.packageName(),
-                                            loc.sourceFileName(),
-                                            String.valueOf(loc.lineNumber())
+                    if (element.getClassName().equals("org.perlonjava.parser.StatementParser") &&
+                            element.getMethodName().equals("parseUseDeclaration")) {
+                        // Artificial caller stack entry created at `use` statement
+                        CallerStack.CallerInfo callerInfo = CallerStack.peek(callerStackIndex.get());
+                        if (callerInfo != null) {
+                            stackTrace.add(
+                                    new ArrayList<>(
+                                            Arrays.asList(
+                                                    callerInfo.packageName(),
+                                                    callerInfo.filename(),
+                                                    String.valueOf(callerInfo.line())
+                                            )
                                     )
-                            )
-                    );
+                            );
+                            callerStackIndex.getAndIncrement(); // Increment the index for the next potential match
+                        }
+                    } else if (element.getClassName().contains("org.perlonjava.anon") ||
+                            element.getClassName().contains("org.perlonjava.perlmodule")) {
+                        // - Compiled Perl methods like:
+                        //     org.perlonjava.anon1.apply(misc/snippets/CallerTest.pm @ CallerTest:36)
+                        // - Perl-like Java methods like:
+                        //     org.perlonjava.perlmodule.Exporter.exportOkTags(Exporter.java:159)
+                        ByteCodeSourceMapper.SourceLocation loc = ByteCodeSourceMapper.parseStackTraceElement(element);
+                        stackTrace.add(
+                                new ArrayList<>(
+                                        Arrays.asList(
+                                                loc.packageName(),
+                                                loc.sourceFileName(),
+                                                String.valueOf(loc.lineNumber())
+                                        )
+                                )
+                        );
+                    }
                 });
+
         return stackTrace;
     }
 }
