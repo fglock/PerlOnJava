c005942b2e79fd7db502b1bacf3b7db7d1f283c9
Flavio S. Glock <flavio.glock@booking.com>
2025-05-22 14:57:57 +0200

    jperl.bat add comments

--- jperl.bat
+++ jperl.bat
@@ -1,3 +1,11 @@
 @echo off
+rem PerlOnJava Launcher Script for Windows
+rem This script launches the PerlOnJava runtime environment, which provides
+rem a Java-based implementation of the Perl programming language.
+rem Repository: github.com/fglock/PerlOnJava
+
+rem Get the directory where this script is located
 set SCRIPT_DIR=%~dp0
-java -Xmx512m -cp "%CLASSPATH%;%SCRIPT_DIR%target/perlonjava-3.0.0.jar" org.perlonjava.Main %*
+
+rem Launch Java
+java -cp "%CLASSPATH%;%SCRIPT_DIR%target\perlonjava-3.0.0.jar" org.perlonjava.Main %*
