b63356852a6d58ebb9009832a36ca9f106ce37fa
Flavio S. Glock <flavio.glock@booking.com>
2025-07-08 11:14:51 +0200

    open pipe - WIP

--- src/main/java/org/perlonjava/io/BytesLayer.java
+++ src/main/java/org/perlonjava/io/BytesLayer.java
@@ -3,13 +3,15 @@ package org.perlonjava.io;
 // Bytes/Raw layer - no transformation
 class BytesLayer implements IOLayer {
     @Override
-    public byte[] processInput(byte[] input) {
-        return input;
+    public void processInput(StreamingContext context) {
+        // Pass through without transformation
+        context.passThrough();
     }
 
     @Override
-    public byte[] processOutput(byte[] output) {
-        return output;
+    public void processOutput(StreamingContext context) {
+        // Pass through without transformation
+        context.passThrough();
     }
 
     @Override
