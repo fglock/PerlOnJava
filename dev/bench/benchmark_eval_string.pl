3331e77b7d99d3bb01d505e08cc6b11b4f423c71
Flavio S. Glock <flavio.glock@booking.com>
2025-07-11 16:12:02 +0200

    fix benchmark

--- dev/bench/benchmark_eval_string.pl
+++ dev/bench/benchmark_eval_string.pl
@@ -4,14 +4,16 @@ use Benchmark;
 
 my $count = 0;
 
+my $k = 0;
 sub loop_with_eval {
-    my $k = 0;
-    eval ( '
+    $k++;
+    my $str = ( '
                 while ( $k < 400 ) {
                     $k = $k + 1;
                     $count = $count + 1;
                 }
     ' . "# " . $k );  # add a difference to avoid caching
+    eval $str;
 }
 
 # Use timethis to benchmark the loop_with_eval subroutine
