7f809b94cb7432b29263867a09aca03f66a0215a
Flavio S. Glock <flavio.glock@booking.com>
2025-07-15 15:29:10 +0200

    tie scalar WIP

--- src/main/java/org/perlonjava/runtime/RuntimeScalarType.java
+++ src/main/java/org/perlonjava/runtime/RuntimeScalarType.java
@@ -14,12 +14,13 @@ public class RuntimeScalarType {
     public static final int GLOB = 6;
     public static final int REGEX = 7;
     public static final int JAVAOBJECT = 8;
+    public static final int TIED_SCALAR = 9;
     // References with bit pattern
-    public static final int CODE = 9 | REFERENCE_BIT;
-    public static final int REFERENCE = 10 | REFERENCE_BIT;
-    public static final int ARRAYREFERENCE = 11 | REFERENCE_BIT;
-    public static final int HASHREFERENCE = 12 | REFERENCE_BIT;
-    public static final int GLOBREFERENCE = 13 | REFERENCE_BIT;
+    public static final int CODE = 100 | REFERENCE_BIT;
+    public static final int REFERENCE = 101 | REFERENCE_BIT;
+    public static final int ARRAYREFERENCE = 102 | REFERENCE_BIT;
+    public static final int HASHREFERENCE = 103 | REFERENCE_BIT;
+    public static final int GLOBREFERENCE = 104 | REFERENCE_BIT;
 
     private RuntimeScalarType() {
     } // Prevent instantiation
