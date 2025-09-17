b1caca7b2bbe8595149bbd3ccc92ae74fbd2caea
Flavio S. Glock <flavio.glock@booking.com>
2025-09-17 09:35:27 +0200

    fix pack() WIP - refactoring

---
+++ src/main/java/org/perlonjava/operators/pack/ParsedModifiers.java
@@ -0,0 +1,8 @@
+package org.perlonjava.operators.pack;
+
+public class ParsedModifiers {
+    public boolean bigEndian;
+    boolean littleEndian;
+    boolean nativeSize;
+    public int endPosition;
+}
