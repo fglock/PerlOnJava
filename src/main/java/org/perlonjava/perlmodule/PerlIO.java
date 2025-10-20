ccb9ebc946207d27a409c6a62b449b7aeb4bfb9b
Flavio S. Glock <flavio.glock@booking.com>
2025-10-20 20:59:48 +0200

    Fix PerlIO::get_layers to accept optional arguments

--- src/main/java/org/perlonjava/perlmodule/PerlIO.java
+++ src/main/java/org/perlonjava/perlmodule/PerlIO.java
@@ -25,7 +25,7 @@ public class PerlIO extends PerlModuleBase {
         PerlIO perlio = new PerlIO();
         try {
             perlio.registerMethod("PerlIO::Layer::find", "find", "$");
-            perlio.registerMethod("get_layers", "$");
+            perlio.registerMethod("get_layers", "$;@");
         } catch (NoSuchMethodException e) {
             System.err.println("Warning: Missing PerlIO method: " + e.getMessage());
         }
@@ -42,13 +42,18 @@ public class PerlIO extends PerlModuleBase {
         return scalarTrue.getList();
     }
 
-    // PerlIO::get_layers($fh) implementation
+    // PerlIO::get_layers($fh, @options) implementation
     // Return the currently applied layers as a string
+    // Optional arguments like 'output', 'details' are accepted but currently ignored
     public static RuntimeList get_layers(RuntimeArray args, int ctx) {
         RuntimeIO fh = args.get(0).getRuntimeIO();
         if (fh instanceof TieHandle) {
             throw new PerlCompilerException("can't get_layers on tied handle");
         }
+        
+        // Parse optional arguments (output => 1, details => 1, etc.)
+        // For now, we ignore these options and just return layer names
+        
         RuntimeArray layers = new RuntimeArray();
         if (fh.ioHandle instanceof LayeredIOHandle layeredIOHandle) {
             for (IOLayer layer : layeredIOHandle.activeLayers) {
