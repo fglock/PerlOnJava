edfc5be1544a5799efb459b056e4be552c5842df
Flavio S. Glock <flavio.glock@booking.com>
2025-10-31 15:00:11 +0100

    Add B.pm stub for Test2 support
    - Provides minimal B module functionality for Test2::Util::Sub
    - Implements B::svref_2object, B::CV, B::GV, B::STASH, B::OP classes
    - Sets $B::INCOMPLETE = 1 to indicate limited introspection
    - No regressions: op/stash.t and uni/stash.t maintain baseline (34/56 and 33/49)
    - Enables Test::Simple to load successfully without hanging

---
+++ src/main/perl/lib/B.pm
@@ -0,0 +1,167 @@
+package B;
+
+# Minimal B module for PerlOnJava
+# Provides basic functionality for Test2
+
+use strict;
+use warnings;
+
+our $VERSION = '1.00_perlonjava';
+
+# Flag indicating this is a stub implementation with limited introspection
+our $INCOMPLETE = 1;
+
+# CV flags
+use constant {
+    CVf_ANON      => 0x0004,
+    CVf_UNIQUE    => 0x0008,
+    CVf_METHOD    => 0x0040,
+    CVf_ISXSUB    => 0x0080,
+};
+
+# Stub classes for B objects
+package B::SV {
+    sub new {
+        my ($class, $ref) = @_;
+        return bless { ref => $ref }, $class;
+    }
+}
+
+package B::CV {
+    our @ISA = ('B::SV');
+    
+    sub GV {
+        my $self = shift;
+        return B::GV->new("__ANON__", "main");
+    }
+    
+    sub STASH {
+        my $self = shift;
+        return B::STASH->new("main");
+    }
+    
+    sub FILE {
+        return "-e";
+    }
+    
+    sub START {
+        return B::OP->new();
+    }
+    
+    sub SV {
+        my $self = shift;
+        return B::SV->new($self->{ref});
+    }
+    
+    sub CvFLAGS {
+        # Always return CVf_ANON for stubs
+        return 0x0004;  # CVf_ANON
+    }
+}
+
+package B::GV {
+    our @ISA = ('B::SV');
+    
+    sub new {
+        my ($class, $name, $pkg) = @_;
+        return bless { name => $name, package => $pkg }, $class;
+    }
+    
+    sub NAME {
+        my $self = shift;
+        return $self->{name};
+    }
+    
+    sub STASH {
+        my $self = shift;
+        return B::STASH->new($self->{package});
+    }
+}
+
+package B::STASH {
+    sub new {
+        my ($class, $pkg) = @_;
+        return bless { package => $pkg }, $class;
+    }
+    
+    sub NAME {
+        my $self = shift;
+        return $self->{package};
+    }
+}
+
+package B::OP {
+    sub new {
+        my $class = shift;
+        return bless { line => 1 }, $class;
+    }
+    
+    sub line {
+        my $self = shift;
+        return $self->{line};
+    }
+    
+    sub next {
+        # Return undef to terminate traversal
+        return;
+    }
+}
+
+package B;
+
+# Main introspection function
+sub svref_2object {
+    my $ref = shift;
+    
+    if (ref($ref) eq 'CODE') {
+        return B::CV->new($ref);
+    }
+    
+    return B::SV->new($ref);
+}
+
+# Export CVf_ANON as a function
+sub CVf_ANON() { return 0x0004; }
+
+# Special SV names
+our @specialsv_name = ('Nullsv', '&PL_sv_undef', '&PL_sv_yes', '&PL_sv_no');
+
+1;
+
+__END__
+
+=head1 NAME
+
+B - Minimal stub implementation for PerlOnJava
+
+=head1 SYNOPSIS
+
+    use B;
+    my $cv = B::svref_2object(\&some_sub);
+    print $cv->GV->NAME;
+
+=head1 DESCRIPTION
+
+This is an incomplete stub implementation of the B module for PerlOnJava.
+It provides minimal functionality for Test2::Util::Sub but cannot provide
+accurate introspection of code references.
+
+The module sets C<$B::INCOMPLETE = 1> to indicate it should not be used 
+for tests requiring full B functionality.
+
+However, the classes and functions are still available for code (like Test2)
+that can work with limited B functionality.
+
+=head1 LIMITATIONS
+
+- Cannot extract actual subroutine names from code references  
+- Cannot determine actual package names
+- CV flags are generic
+- File locations are not tracked
+
+=cut
+
+# Note: In a complete Perl implementation, this module would return false (0;)
+# to make tests skip B-dependent functionality, but PerlOnJava's require doesn't
+# check return values yet. The module is intentionally incomplete.
+1;
