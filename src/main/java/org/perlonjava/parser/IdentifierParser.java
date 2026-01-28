0d343e61b61fd38a2cc312a8ef1b34e710881d96
Flavio Soibelmann Glock <fglock@gmail.com>
2026-01-28 14:29:02 +0100

    Fix lexsub.t - allow ) after :: in package names
    - Add ) to allowed tokens after :: in parseSubroutineIdentifier
    - This allows package names like 'o::' to be used in function calls like bless([], o::)
    - Fixes op/lexsub.t which was failing with 'Bad name after o::'

--- src/main/java/org/perlonjava/parser/IdentifierParser.java
+++ src/main/java/org/perlonjava/parser/IdentifierParser.java
@@ -459,7 +459,7 @@ public class IdentifierParser {
                 if (token.type != LexerTokenType.IDENTIFIER && token.type != LexerTokenType.NUMBER && 
                     !token.text.equals("'") && !token.text.equals("::") && !token.text.equals("->") &&
                     token.type != LexerTokenType.EOF &&
-                    !(token.type == LexerTokenType.OPERATOR && (token.text.equals("}") || token.text.equals(";") || token.text.equals("=")))) {
+                    !(token.type == LexerTokenType.OPERATOR && (token.text.equals("}") || token.text.equals(";") || token.text.equals("=") || token.text.equals(")")))) {
                     // Bad name after ::
                     parser.throwCleanError("Bad name after " + variableName.toString() + "::");
                 }
